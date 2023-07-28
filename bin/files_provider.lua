b0ac73244ac4ecf78671e29bffd07c538a86b3b6
linrongbin16 <linrongbin16@outlook.com>
2023-07-28 13:44:50 +0800

    feat: debug command

--- bin/files_provider.lua
+++ bin/files_provider.lua
@@ -1,20 +1,23 @@
 local _FZFX_DEBUG = os.getenv("_FZFX_DEBUG")
 local _FZFX_FILES_PROVIDER = os.getenv("_FZFX_FILES_PROVIDER")
 if _FZFX_DEBUG then
-    print(
-        string.format("DEBUG _FZFX_FILES_PROVIDER:[%s]\n", _FZFX_FILES_PROVIDER)
+    os.execute(
+        string.format(
+            'echo "DEBUG _FZFX_FILES_PROVIDER:[%s]"',
+            _FZFX_FILES_PROVIDER
+        )
     )
 end
 local f = io.open(_FZFX_FILES_PROVIDER --[[@as string]], "r")
 assert(
     f ~= nil,
     string.format(
-        "Error! failed to open _FZFX_FILES_PROVIDER:%s",
+        "error! failed to open _FZFX_FILES_PROVIDER:%s",
         _FZFX_FILES_PROVIDER
     )
 )
 local cmd = vim.fn.trim(f:read("*a"))
 if _FZFX_DEBUG then
-    print(string.format("DEBUG cmd:[%s]\n", cmd))
+    os.execute(string.format('echo "DEBUG cmd:[%s]"', cmd))
 end
 os.execute(cmd)
--- bin/live_grep_provider.lua
+++ bin/live_grep_provider.lua
@@ -1,8 +1,28 @@
+local _FZFX_DEBUG = os.getenv("_FZFX_DEBUG")
+local _FZFX_LIVE_GREP_PROVIDER = os.getenv("_FZFX_LIVE_GREP_PROVIDER")
 local args = _G.arg
-
-print("trim:" .. vim.fn.trim(" asdf "))
-local _FZFX_RG_COMMAND = os.getenv("_FZFX_RG_COMMAND")
 local content = args[1]
-local cmd = string.format('%s -- "%s"', _FZFX_RG_COMMAND, content)
-print("cmd:[" .. cmd .. "]\n")
+
+if _FZFX_DEBUG then
+    os.execute(
+        string.format(
+            'echo "DEBUG _FZFX_LIVE_GREP_PROVIDER:[%s]"',
+            _FZFX_LIVE_GREP_PROVIDER
+        )
+    )
+    os.execute(string.format('echo "DEBUG content:[%s]"', content))
+end
+
+local f = io.open(_FZFX_LIVE_GREP_PROVIDER --[[@as string]], "r")
+assert(
+    f ~= nil,
+    string.format(
+        "error! failed to open _FZFX_LIVE_GREP_PROVIDER:%s",
+        _FZFX_LIVE_GREP_PROVIDER
+    )
+)
+local cmd = vim.fn.trim(f:read("*a"))
+if _FZFX_DEBUG then
+    print(string.format("DEBUG cmd:[%s]\n", cmd))
+end
 os.execute(cmd)
