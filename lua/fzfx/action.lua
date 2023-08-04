2e31d748a6ca3a9125df8a8f7e0934f02a9aa2ac
linrongbin16 <linrongbin16@outlook.com>
2023-08-04 22:19:39 +0800

    fix: edit rg action

--- lua/fzfx/action.lua
+++ lua/fzfx/action.lua
@@ -14,15 +14,27 @@ local function edit_rg(lines)
     for i, line in ipairs(lines) do
         local splits = vim.split(line, ":")
         local filename = splits[1]
-        local cmd = string.format("edit %s", vim.fn.expand(filename))
+        local row = tonumber(splits[2])
+        local col = tonumber(splits[3])
+        local edit_cmd = string.format("edit %s", vim.fn.expand(filename))
+        local setpos_cmd =
+            string.format("call setpos('.', [0, %d, %d])", row, col)
         log.debug(
-            "|fzfx.action - edit_rg| line[%d] - splits:%s, filename:%s, cmd:%s",
+            "|fzfx.action - edit_rg| line[%d] - splits:%s, filename:%s, row:%d, col:%d",
             i,
             vim.inspect(splits),
             vim.inspect(filename),
-            vim.inspect(cmd)
+            vim.inspect(row),
+            vim.inspect(col)
         )
-        vim.cmd(cmd)
+        log.debug(
+            "|fzfx.action - edit_rg| line[%d] - edit_cmd:%s, setpos_cmd:%s",
+            i,
+            vim.inspect(edit_cmd),
+            vim.inspect(setpos_cmd)
+        )
+        vim.cmd(edit_cmd)
+        vim.cmd(setpos_cmd)
     end
 end
 
