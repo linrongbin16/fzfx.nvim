d38208f52f317964d94f6ab2b2739fb436a8409b
linrongbin16 <linrongbin16@outlook.com>
2023-08-11 13:35:11 +0800

    WIP

--- bin/rpc/client.lua
+++ bin/rpc/client.lua
@@ -1,48 +1,39 @@
-local debug_enable = tostring(vim.env._FZFX_NVIM_DEBUG_ENABLE):lower() == "1"
-
-local args = _G.arg
-local sockaddr = args[1]
-local function_id = args[2]
-
-if debug_enable then
-    io.write(string.format("DEBUG sockaddr:%s\n", vim.inspect(sockaddr)))
-    io.write(string.format("DEBUG function_id:%s\n", vim.inspect(function_id)))
-end
-
---- @param chanid integer
---- @param data string
---- @param name string
---- @return nil
-local function accept(chanid, data, name)
-    if debug_enable then
-        io.write(string.format("DEBUG accept.chanid:%s\n", vim.inspect(chanid)))
-        io.write(string.format("DEBUG accept.data:%s\n", vim.inspect(data)))
-        io.write(string.format("DEBUG accept.name:%s\n", vim.inspect(name)))
-    end
-end
-
-local socket_channel_id = vim.fn.sockconnect(
-    "tcp",
-    sockaddr,
-    { on_data = accept, data_buffered = true }
-)
-
-if socket_channel_id > 0 then
-    local bytes = vim.fn.chansend(socket_channel_id, { function_id, "request" })
-    if bytes == 0 then
-        io.stderr:write(
-            string.format(
-                "|fzfx.ipc_client| error! failed to send any bytes server on socket address: %s, channel id: %s",
-                vim.inspect(sockaddr),
-                vim.inspect(socket_channel_id)
-            )
-        )
-    end
-else
-    io.stderr:write(
-        string.format(
-            "|fzfx.ipc_client| error! failed to connect to server on socket address: %s",
-            vim.inspect(sockaddr)
-        )
+local SELF_PATH = vim.env._FZFX_NVIM_SELF_PATH
+if type(SELF_PATH) ~= "string" or string.len(SELF_PATH) == 0 then
+    io.write(
+        string.format("|fzfx.bin.files.provider| error! SELF_PATH is empty!")
     )
 end
+vim.opt.runtimepath:append(SELF_PATH)
+local shell_helpers = require("fzfx.shell_helpers")
+
+local SOCKET_ADDRESS = vim.env._FZFX_NVIM_SOCKET_ADDRESS
+shell_helpers.log_ensure(
+    type(SOCKET_ADDRESS) == "string" and string.len(SOCKET_ADDRESS) > 0,
+    "|fzfx.bin.rpc.client| error! SOCKET_ADDRESS must not be empty!"
+)
+local registry_id = _G.arg[1]
+
+shell_helpers.log_debug("SOCKET_ADDRESS:%s", vim.inspect(SOCKET_ADDRESS))
+shell_helpers.log_debug("registry_id:%s", vim.inspect(registry_id))
+
+local channel_id = vim.fn.sockconnect("tcp", SOCKET_ADDRESS, { rpc = true })
+shell_helpers.log_debug("channel_id:%s", vim.inspect(channel_id))
+shell_helpers.log_ensure(
+    channel_id > 0,
+    "|fzfx.bin.rpc.client| error! failed to connect socket on SOCKET_ADDRESS:%s",
+    vim.inspect(SOCKET_ADDRESS)
+)
+vim.rpcrequest(
+    channel_id,
+    "nvim_exec_lua",
+    [[
+    local luaargs = {...}
+    local registry_id = luaargs[1]
+    require("fzfx.rpc_helpers").call(registry_id)
+    ]],
+    {
+        registry_id,
+    }
+)
+vim.fn.chanclose(channel_id)
