60a9892bdf3495019ef99cbbea345fd4d4559857
linrongbin16 <linrongbin16@outlook.com>
2023-08-12 18:09:06 +0800

    fix: log

--- lua/fzfx/helpers.lua
+++ lua/fzfx/helpers.lua
@@ -19,13 +19,13 @@ local function get_visual_lines(mode)
     column_start = math.min(column_start, column_end)
     column_end = math.max(column_start, column_end)
     log.debug(
-        "|fzfx.utils - get_visual_lines| mode:%s, start_pos:%s, end_pos:%s",
+        "|fzfx.helpers - get_visual_lines| mode:%s, start_pos:%s, end_pos:%s",
         vim.inspect(mode),
         vim.inspect(start_pos),
         vim.inspect(end_pos)
     )
     log.debug(
-        "|fzfx.utils - get_visual_lines| line_start:%s, line_end:%s, column_start:%s, column_end:%s",
+        "|fzfx.helper - get_visual_lines| line_start:%s, line_end:%s, column_start:%s, column_end:%s",
         vim.inspect(line_start),
         vim.inspect(line_end),
         vim.inspect(column_start),
@@ -34,7 +34,7 @@ local function get_visual_lines(mode)
 
     local lines = vim.api.nvim_buf_get_lines(0, line_start - 1, line_end, false)
     if #lines == 0 then
-        log.debug("|fzfx.utils - get_visual_lines| empty lines")
+        log.debug("|fzfx.helpers - get_visual_lines| empty lines")
         return ""
     end
 
@@ -42,7 +42,7 @@ local function get_visual_lines(mode)
     local cursor_line = cursor_pos[2]
     local cursor_column = cursor_pos[3]
     log.debug(
-        "|fzfx.utils - get_visual_lines| cursor_pos:%s, cursor_line:%s, cursor_column:%s",
+        "|fzfx.helpers - get_visual_lines| cursor_pos:%s, cursor_line:%s, cursor_column:%s",
         vim.inspect(cursor_pos),
         vim.inspect(cursor_line),
         vim.inspect(cursor_column)
@@ -52,7 +52,7 @@ local function get_visual_lines(mode)
         lines[#lines] = string.sub(lines[#lines], 1, column_end - offset + 1)
         lines[1] = string.sub(lines[1], column_start)
         log.debug(
-            "|fzfx.utils - get_visual_lines| v or \\22, lines:%s",
+            "|fzfx.helpers - get_visual_lines| v or \\22, lines:%s",
             vim.inspect(lines)
         )
     elseif mode == "V" then
@@ -60,7 +60,7 @@ local function get_visual_lines(mode)
             lines[1] = vim.fn.trim(lines[1])
         end
         log.debug(
-            "|fzfx.utils - get_visual_lines| V, lines:%s",
+            "|fzfx.helpers - get_visual_lines| V, lines:%s",
             vim.inspect(lines)
         )
     end
