60f872b8290487232fabe34cefb28814a66cf258
linrongbin16 <linrongbin16@outlook.com>
2023-08-14 12:44:31 +0800

    refactor: move exit insert mode to restore

--- lua/fzfx/popup.lua
+++ lua/fzfx/popup.lua
@@ -127,6 +127,10 @@ function SavedWindowContext:restore()
             vim.inspect(self.winnr)
         )
     end
+
+    -- exit insert mode if any
+    local esc = vim.api.nvim_replace_termcodes("<ESC>", true, false, true)
+    vim.api.nvim_feedkeys(esc, "x", false)
 end
 
 --- @class Popup
