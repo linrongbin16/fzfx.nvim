1b9b9e450daa76731bc3b16c3252649a9030257d
linrongbin16 <linrongbin16@outlook.com>
2023-08-17 23:20:46 +0800

    refactor: new framework

---
+++ lua/fzfx/files2.lua
@@ -0,0 +1,152 @@
+local Pipeline = require("fzfx.schema").Pipeline
+local Schema = require("fzfx.schema").Schema
+local NormalCommandOpts = require("fzfx.schema").NormalCommandOpts
+local constants = require("fzfx.constants")
+local env = require("fzfx.env")
+local color = require("fzfx.color")
+local action = require("fzfx.action")
+local fzf_opt_constants = require("fzfx.fzf_opt_constants")
+
+local default_fd_command =
+    string.format("%s -cnever -tf -tl -L -i", constants.fd)
+
+--- @return string
+local function restricted_provider()
+    return default_fd_command
+end
+
+--- @return string
+local function unrestricted_provider()
+    return default_fd_command .. " -u"
+end
+
+--- @param line string
+--- @return string
+local function processor(line)
+    if env.icon_enable() then
+        local devicons = require("nvim-web-devicons")
+        local ext = vim.fn.fnamemodify(line, ":e")
+        local icon, icon_color = devicons.get_icon_color(line, ext)
+        -- if DEBUG_ENABLE then
+        --     log_debug(
+        --         "|fzfx.shell_helpers - render_line_with_icon| line:%s, ext:%s, icon:%s, color:%s\n",
+        --         vim.inspect(line),
+        --         vim.inspect(ext),
+        --         vim.inspect(icon),
+        --         vim.inspect(color)
+        --     )
+        -- end
+        if type(icon) == "string" and string.len(icon) > 0 then
+            local colorfmt = color.csi(icon_color, true)
+            if colorfmt then
+                return string.format("[%sm%s[0m %s", colorfmt, icon, line)
+            else
+                return string.format("%s %s", icon, line)
+            end
+        else
+            if vim.fn.isdirectory(line) > 0 then
+                return string.format(
+                    "%s %s",
+                    require("fzfx.config").get_config().popup.icon.folder,
+                    line
+                )
+            else
+                return string.format(
+                    "%s %s",
+                    require("fzfx.config").get_config().popup.icon.unknown_file,
+                    line
+                )
+            end
+        end
+    else
+        return line
+    end
+end
+
+local function previewer(...)
+    local args = { ... }
+    local filename = args[1]
+    local lineno = nil
+    if #args >= 2 then
+        lineno = args[2]
+    end
+
+    if env.icon_enable() then
+        local splits = vim.fn.split(filename)
+        filename = splits[2]
+    end
+
+    if vim.fn.executable("batcat") > 0 or vim.fn.executable("bat") > 0 then
+        local style = "numbers,changes"
+        if
+            type(vim.env["BAT_STYLE"]) == "string"
+            and string.len(vim.env["BAT_STYLE"]) > 0
+        then
+            style = vim.env["BAT_STYLE"]
+        end
+        local cmd = string.format(
+            "%s --style=%s --color=always --pager=never %s -- %s",
+            constants.bat,
+            style,
+            (lineno ~= nil and string.len(lineno) > 0)
+                    and string.format("--highlight-line=%s", lineno)
+                or "",
+            filename
+        )
+
+        return cmd
+    else
+        local cmd = string.format("cat %s", filename)
+        return cmd
+    end
+end
+
+local restricted_pipeline = Pipeline:make({
+    provider = restricted_provider,
+    line_processor = processor,
+    previewer = previewer,
+    help_format = "%s to restricted mode",
+})
+
+local unrestricted_pipeline = Pipeline:make({
+    provider = unrestricted_provider,
+    line_processor = processor,
+    previewer = previewer,
+    help_format = "%s to unrestricted mode",
+})
+
+local default_fzf_opts = {
+    fzf_opt_constants.multi,
+    fzf_opt_constants.toggle,
+    fzf_opt_constants.toggle_all,
+    fzf_opt_constants.preview_half_page_down,
+    fzf_opt_constants.preview_half_page_up,
+    fzf_opt_constants.toggle_preview,
+}
+
+local files = Schema:make({
+    name = "FzfxFiles",
+    pipelines = {
+        ["ctrl-r"] = restricted_pipeline,
+        ["ctrl-u"] = unrestricted_pipeline,
+    },
+    default_pipeline = restricted_pipeline,
+    interactive_actions = nil,
+    expect_actions = {
+        ["esc"] = action.nop,
+        ["enter"] = action.edit,
+        ["double-click"] = action.edit,
+    },
+    command_opts = NormalCommandOpts:make({
+        desc = "Find files",
+        complete = "dir",
+    }),
+    feed_method = "args",
+    fzf_opts = default_fzf_opts,
+})
+
+local M = {
+    files = files,
+}
+
+return M
---
+++ lua/fzfx/fzf_opt_constants.lua
@@ -0,0 +1,11 @@
+local M = {
+    multi = "--multi",
+    toggle = "--bind=ctrl-e:toggle",
+    toggle_all = "--bind=ctrl-a:toggle-all",
+    toggle_preview = "--bind=alt-p:toggle-preview",
+    preview_half_page_down = "--bind=ctrl-f:preview-half-page-down",
+    preview_half_page_up = "--bind=ctrl-b:preview-half-page-up",
+    no_multi = "--no-multi",
+}
+
+return M
