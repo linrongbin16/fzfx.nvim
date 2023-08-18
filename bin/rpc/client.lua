c73f17897798dc803ae510cfed1f2a7ddb2e6abd
linrongbin16 <linrongbin16@outlook.com>
2023-08-18 23:08:26 +0800

    fix: log

--- bin/rpc/client.lua
+++ bin/rpc/client.lua
@@ -1,8 +1,6 @@
 local SELF_PATH = vim.env._FZFX_NVIM_SELF_PATH
 if type(SELF_PATH) ~= "string" or string.len(SELF_PATH) == 0 then
-    io.write(
-        string.format("|fzfx.bin.files.provider| error! SELF_PATH is empty!")
-    )
+    io.write(string.format("|fzfx.bin.rpc.client| error! SELF_PATH is empty!"))
 end
 vim.opt.runtimepath:append(SELF_PATH)
 local shell_helpers = require("fzfx.shell_helpers")
@@ -20,6 +18,7 @@ end
 
 shell_helpers.log_debug("SOCKET_ADDRESS:%s", vim.inspect(SOCKET_ADDRESS))
 shell_helpers.log_debug("registry_id:%s", vim.inspect(registry_id))
+shell_helpers.log_debug("params:%s", vim.inspect(params))
 
 local channel_id = vim.fn.sockconnect("pipe", SOCKET_ADDRESS, { rpc = true })
 shell_helpers.log_debug("channel_id:%s", vim.inspect(channel_id))
