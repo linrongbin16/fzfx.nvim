9c6ea916d19708c2b58d19a43bfb9cd5f723cd5b
linrongbin16 <linrongbin16@outlook.com>
2023-08-22 11:46:30 +0800

    fix: depress window error

--- lua/fzfx/popup.lua
+++ lua/fzfx/popup.lua
@@ -115,7 +115,7 @@ function SavedWindowContext:restore()
     if vim.api.nvim_tabpage_is_valid(self.tabnr) then
         vim.api.nvim_set_current_tabpage(self.tabnr)
     else
-        log.info(
+        log.debug(
             "error! cannot restore to invalid previous tabpage! %s",
             vim.inspect(self.tabnr)
         )
@@ -123,7 +123,7 @@ function SavedWindowContext:restore()
     if vim.api.nvim_win_is_valid(self.winnr) then
         vim.api.nvim_set_current_win(self.winnr)
     else
-        log.info(
+        log.debug(
             "error! cannot restore to invalid previous window! %s",
             vim.inspect(self.winnr)
         )
@@ -314,7 +314,7 @@ function Popup:close()
     if vim.api.nvim_win_is_valid(self.winnr) then
         vim.api.nvim_win_close(self.winnr, true)
     else
-        log.info(
+        log.debug(
             "error! cannot close invalid popup window! %s",
             vim.inspect(self.winnr)
         )
