2abef2cbb276aa85b89254affa0989780db74127
linrongbin16 <linrongbin16@outlook.com>
2023-08-25 12:33:35 +0800

    revert

--- lua/fzfx/utils.lua
+++ lua/fzfx/utils.lua
@@ -49,8 +49,8 @@ end
 --- @return boolean
 local function is_buf_valid(bufnr)
     local bufname = vim.api.nvim_buf_get_name(bufnr)
-    -- return vim.api.nvim_buf_is_valid(bufnr)
-    return vim.fn.buflisted(bufnr) > 0
+    return vim.api.nvim_buf_is_valid(bufnr)
+        and vim.fn.buflisted(bufnr) > 0
         and type(bufname) == "string"
         and string.len(bufname) > 0
 end
