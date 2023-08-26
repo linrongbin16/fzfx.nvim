eab07bdb0435092331a4d455a6b12a3b7a044df7
linrongbin16 <linrongbin16@outlook.com>
2023-08-26 17:23:37 +0800

    perf: HeaderSwitch

--- lua/fzfx/general.lua
+++ lua/fzfx/general.lua
@@ -224,6 +224,74 @@ end
 
 -- previewer switch }
 
+-- header switch {
+
+--- @class HeaderSwitch
+--- @field pipeline PipelineName?
+--- @field headers table<PipelineName, string[]>?
+local HeaderSwitch = {
+    pipeline = nil,
+    headers = nil,
+}
+
+--- @param pipeline PipelineName
+--- @param provider_configs Configs
+--- @return HeaderSwitch
+function HeaderSwitch:new(pipeline, provider_configs)
+    local headers = {}
+    for provider_name, provider_opts in pairs(provider_configs) do
+        local switch_help = {}
+        for provider_name2, provider_opts2 in pairs(provider_configs) do
+            local switch_key2 = string.lower(provider_opts2.key)
+            if provider_name2 ~= provider_name then
+                table.insert(
+                    switch_help,
+                    color.render(
+                        "%s to "
+                            .. table.concat(
+                                vim.fn.split(provider_name2, "_"),
+                                " "
+                            ),
+                        color.magenta,
+                        string.upper(switch_key2)
+                    )
+                )
+            end
+        end
+        headers[provider_name] = switch_help
+    end
+    return vim.tbl_deep_extend("force", vim.deepcopy(HeaderSwitch), {
+        pipeline = pipeline,
+        headers = headers,
+    })
+end
+
+--- @param next_pipeline PipelineName
+--- @return nil
+function HeaderSwitch:switch(next_pipeline)
+    log.ensure(
+        type(self.headers[next_pipeline]) == "table",
+        "|fzfx.general - HeaderSwitch:get_header| next pipeline (%s) must exists in headers! %s",
+        vim.inspect(next_pipeline),
+        vim.inspect(self)
+    )
+    self.pipeline = next_pipeline
+end
+
+--- @return FzfOpt?
+function HeaderSwitch:get_header()
+    log.ensure(
+        type(self.headers[self.pipeline]) == "table",
+        "|fzfx.general - HeaderSwitch:get_header| pipeline (%s) must exists in headers! %s",
+        vim.inspect(self.pipeline),
+        vim.inspect(self)
+    )
+    local switch_help = self.headers[self.pipeline]
+    return string.format(":: Press %s", table.concat(switch_help, ", "))
+end
+
+-- header switch }
+
 --- @param pipeline_configs Configs
 local function get_pipeline_size(pipeline_configs)
     local n = 0
@@ -319,58 +387,49 @@ local function general(name, query, bang, pipeline_configs, default_pipeline)
         },
     }
 
-    -- when only have 1 pipeline, no need to add help message and switch keys
-    if pipeline_size > 1 then
-        local header_builder = {}
-        for pipeline, provider_opts in pairs(pipeline_configs.providers) do
-            local switch_pipeline_key = string.lower(provider_opts.key)
-            table.insert(
-                header_builder,
-                color.render(
-                    "%s to %s",
-                    color.magenta,
-                    string.upper(switch_pipeline_key),
-                    table.concat(vim.fn.split(pipeline, "_"), " ")
-                )
-            )
-        end
-        local headers = ":: Press " .. table.concat(header_builder, ", ")
+    local header_switch =
+        HeaderSwitch:new(default_pipeline, pipeline_configs.providers)
+    local switch_rpc_registries = {}
 
+    -- when only have 1 pipeline, no need to add help for switch keys
+    if pipeline_size > 1 then
+        local header = header_switch:get_header()
         table.insert(fzf_opts, {
             "--header",
-            headers,
+            header,
         })
 
         for pipeline, provider_opts in pairs(pipeline_configs.providers) do
-            local switch_pipeline_key = string.lower(provider_opts.key)
+            local switch_key = string.lower(provider_opts.key)
 
-            local function switch_pipeline_rpc()
+            local function switch_rpc()
                 provider_switch:switch(pipeline)
                 previewer_switch:switch(pipeline)
+                header_switch:switch(pipeline)
             end
 
-            local switch_pipeline_registry_id =
-                server.get_global_rpc_server():register(switch_pipeline_rpc)
+            local switch_rpc_registry_id =
+                server.get_global_rpc_server():register(switch_rpc)
+            table.insert(switch_rpc_registries, switch_rpc_registry_id)
 
             local switch_pipeline_command = string.format(
                 "%s %s",
                 shell.make_lua_command("rpc", "client.lua"),
-                switch_pipeline_registry_id
+                switch_rpc_registry_id
             )
             local bind_builder = string.format(
                 "%s:unbind(%s)+execute-silent(%s)+change-header(%s)+reload(%s)",
-                switch_pipeline_key,
-                switch_pipeline_key,
+                switch_key,
+                switch_key,
                 switch_pipeline_command,
-                headers,
+                header_switch:get_header(),
                 reload_query_command
             )
             for pipeline2, provider_opts2 in pairs(pipeline_configs.providers) do
                 if pipeline2 ~= pipeline then
-                    local switch_pipeline_key2 =
-                        string.lower(provider_opts2.key)
+                    local switch_key2 = string.lower(provider_opts2.key)
                     bind_builder = bind_builder
-                        .. string.format("+rebind(%s)", switch_pipeline_key2)
+                        .. string.format("+rebind(%s)", switch_key2)
                 end
             end
             table.insert(fzf_opts, {
@@ -396,7 +455,11 @@ local function general(name, query, bang, pipeline_configs, default_pipeline)
         fzf_opts,
         actions,
         function()
+            server.get_global_rpc_server():unregister(provide_rpc_registry_id)
             server.get_global_rpc_server():unregister(preview_rpc_registry_id)
+            for _, switch_registry_id in ipairs(switch_rpc_registries) do
+                server.get_global_rpc_server():unregister(switch_registry_id)
+            end
         end
     )
     return p
