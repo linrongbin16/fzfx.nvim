a31882f05cab9ebc4f23faabda8469b1dbc07547
linrongbin16 <linrongbin16@outlook.com>
2023-08-27 15:55:25 +0800

    test: git commits config migration

--- lua/tests/minimal_git_commits_config_deprecate_init.lua
+++ lua/tests/minimal_git_commits_config_deprecate_init.lua
@@ -22,6 +22,20 @@ local opts = {
     defaults = { lazy = false },
 }
 
+--- @type table<string, FzfOpt>
+local default_fzf_options = {
+    multi = "--multi",
+    toggle = "--bind=ctrl-e:toggle",
+    toggle_all = "--bind=ctrl-a:toggle-all",
+    toggle_preview = "--bind=alt-p:toggle-preview",
+    preview_half_page_down = "--bind=ctrl-f:preview-half-page-down",
+    preview_half_page_up = "--bind=ctrl-b:preview-half-page-up",
+    no_multi = "--no-multi",
+}
+
+local default_git_log_pretty =
+    "%C(yellow)%h %C(cyan)%cd %C(green)%aN%C(auto)%d %Creset%s"
+
 require("lazy").setup({
     "folke/tokyonight.nvim",
     "nvim-tree/nvim-web-devicons",
@@ -33,7 +47,130 @@ require("lazy").setup({
         "linrongbin16/fzfx.nvim",
         dev = true,
         dir = "~/github/linrongbin16/fzfx.nvim",
-        opts = {},
+        config = function()
+            require("fzfx").setup({
+                git_commits = {
+                    commands = {
+                        -- normal
+                        {
+                            name = "FzfxGCommits",
+                            feed = require("fzfx.meta").CommandFeedEnum.ARGS,
+                            opts = {
+                                bang = true,
+                                nargs = "?",
+                                desc = "Search git commits",
+                            },
+                            default_provider = "all_commits",
+                        },
+                        {
+                            name = "FzfxGCommitsB",
+                            feed = require("fzfx.meta").CommandFeedEnum.ARGS,
+                            opts = {
+                                bang = true,
+                                nargs = "?",
+                                desc = "Search git commits only on current buffer",
+                            },
+                            default_provider = "buffer_commits",
+                        },
+                        -- visual
+                        {
+                            name = "FzfxGCommitsV",
+                            feed = require("fzfx.meta").CommandFeedEnum.VISUAL,
+                            opts = {
+                                bang = true,
+                                range = true,
+                                desc = "Search git commits by visual select",
+                            },
+                            default_provider = "all_commits",
+                        },
+                        {
+                            name = "FzfxGCommitsBV",
+                            feed = require("fzfx.meta").CommandFeedEnum.VISUAL,
+                            opts = {
+                                bang = true,
+                                range = true,
+                                desc = "Search git commits only on current buffer by visual select",
+                            },
+                            default_provider = "buffer_commits",
+                        },
+                        -- cword
+                        {
+                            name = "FzfxGCommitsW",
+                            feed = require("fzfx.meta").CommandFeedEnum.CWORD,
+                            opts = {
+                                bang = true,
+                                desc = "Search git commits by cursor word",
+                            },
+                            default_provider = "all_commits",
+                        },
+                        {
+                            name = "FzfxGCommitsBW",
+                            feed = require("fzfx.meta").CommandFeedEnum.CWORD,
+                            opts = {
+                                bang = true,
+                                desc = "Search git commits only on current buffer by cursor word",
+                            },
+                            default_provider = "buffer_commits",
+                        },
+                        -- put
+                        {
+                            name = "FzfxGCommitsP",
+                            feed = require("fzfx.meta").CommandFeedEnum.PUT,
+                            opts = {
+                                bang = true,
+                                desc = "Search git commits by yank text",
+                            },
+                            default_provider = "all_commits",
+                        },
+                        {
+                            name = "FzfxGCommitsBP",
+                            feed = require("fzfx.meta").CommandFeedEnum.PUT,
+                            opts = {
+                                bang = true,
+                                desc = "Search git commits only on current buffer by yank text",
+                            },
+                            default_provider = "buffer_commits",
+                        },
+                    },
+                    providers = {
+                        all_commits = {
+                            "ctrl-a",
+                            string.format(
+                                "git log --pretty=%s --date=short --color=always",
+                                require("fzfx.utils").shellescape(
+                                    default_git_log_pretty
+                                )
+                            ),
+                        },
+                        buffer_commits = {
+                            "ctrl-u",
+                            string.format(
+                                "git log --pretty=%s --date=short --color=always",
+                                require("fzfx.utils").shellescape(
+                                    default_git_log_pretty
+                                )
+                            ),
+                        },
+                    },
+                    previewers = "git show --color=always",
+                    actions = {
+                        ["esc"] = require("fzfx.actions").nop,
+                        ["enter"] = require("fzfx.actions").yank_git_commit,
+                        ["double-click"] = require("fzfx.actions").yank_git_commit,
+                    },
+                    fzf_opts = {
+                        default_fzf_options.no_multi,
+                        default_fzf_options.preview_half_page_down,
+                        default_fzf_options.preview_half_page_up,
+                        default_fzf_options.toggle_preview,
+                        {
+                            "--prompt",
+                            "GCommits > ",
+                        },
+                    },
+                },
+            })
+        end,
         dependencies = {
             "folke/tokyonight.nvim",
             "nvim-tree/nvim-web-devicons",
