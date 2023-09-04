a4f8eadfabb997aac9a407c33173a92b03ef68da
linrongbin16 <linrongbin16@outlook.com>
2023-09-04 09:50:11 +0800

    perf: batch fs_read

--- bin/general/provider.lua
+++ bin/general/provider.lua
@@ -274,112 +274,83 @@ then
         vim.loop.run()
     end
 elseif metajson.provider_type == "list" then
-    local open_result = vim.loop.fs_open(
-        resultfile,
-        "r",
-        438,
-        --- @param open_err string?
-        --- @param fd integer
-        function(open_err, fd)
-            shell_helpers.log_ensure(
-                not open_err,
-                "|provider| error! failed to fs_open list provider resultfile (%s): %s",
-                vim.inspect(resultfile),
-                vim.inspect(open_err)
-            )
-            vim.loop.fs_fstat(
-                fd,
-                --- @param fstat_err string?
-                --- @param stat table
-                function(fstat_err, stat)
-                    shell_helpers.log_ensure(
-                        not fstat_err,
-                        "|provdier| error! failed to fs_fstat list provider resultfile (%s): %s",
-                        vim.inspect(resultfile),
-                        vim.inspect(fstat_err)
-                    )
-                    local data_buffer = nil
-                    local filesize = stat.size
-                    local batchsize = 4096
-                    local offset = 0
-
-                    --- @param exit_err string?
-                    local function on_exit(exit_err)
-                        local code = 0
-                        if exit_err then
-                            shell_helpers.log_debug(
-                                "|provider| list provider exit with resultfile(%s): %s",
-                                vim.inspect(resultfile),
-                                vim.inspect(exit_err)
-                            )
-                            code = 130
-                        end
-                        vim.loop.fs_close(fd, function(close_err)
-                            if close_err then
-                                shell_helpers.log_debug(
-                                    "|provider| error! failed to fs_close(1) list provider resultfile (%s): %s",
-                                    vim.inspect(resultfile),
-                                    vim.inspect(close_err)
-                                )
-                                code = 130
-                            end
-                            os.exit(code)
-                        end)
-                    end
-
-                    --- @param err string?
-                    --- @param data string?
-                    local function on_output(err, data)
-                        if err then
-                            shell_helpers.log_debug(
-                                "|provider| error! failed to fs_read list provider resultfile (%s): %s",
-                                vim.inspect(resultfile),
-                                vim.inspect(err)
-                            )
-                            on_exit(err)
-                            return
-                        end
-                        if not data then
-                            if data_buffer then
-                            end
-                            on_exit()
-                            return
-                        end
-
-                        -- append data to data_buffer
-                        data_buffer = data_buffer and (data_buffer .. data)
-                            or data
-                        -- foreach the data_buffer and find every line
-                        local i = 1
-                        while i <= #data_buffer do
-                            local newline_pos =
-                                shell_helpers.string_find(data_buffer, "\n", i)
-                            if not newline_pos then
-                                break
-                            end
-                            local line = data_buffer:sub(i, newline_pos)
-                            println(line)
-                            i = newline_pos + 1
-                        end
-                        -- truncate the printed lines if found any
-                        data_buffer = i <= #data_buffer
-                                and data_buffer:sub(i, #data_buffer)
-                            or nil
-
-                        offset = offset + #data
-                    end
-
-                    vim.loop.fs_read(fd, batchsize, 0, on_output)
-                end
-            )
-        end
-    )
+    local fd = vim.loop.fs_open(resultfile, "r", 438) --[[@as integer]]
     shell_helpers.log_ensure(
-        open_result ~= nil,
-        "|provider| failed to fs_open resultfile(%s): %s",
-        vim.inspect(resultfile),
-        vim.inspect(open_result)
+        type(fd) == "number",
+        "|provider| error! failed to fs_open resultfile: %s",
+        vim.inspect(resultfile)
     )
+    local stat = vim.loop.fs_fstat(fd) --[[@as table]]
+    shell_helpers.log_ensure(
+        type(stat) == "table",
+        "|provider| error! failed to fs_open list provider resultfile: %s",
+        vim.inspect(resultfile)
+    )
+
+    --- @type string?
+    local data_buffer = nil
+    local filesize = stat.size
+    local batchsize = 4096
+    local offset = 0
+    local code = 0
+
+    while true do
+        local data, --[[@as string?]]
+            read_err,
+            read_name =
+            vim.loop.fs_read(fd, batchsize, offset)
+        if not data then
+            if read_err then
+                shell_helpers.log_err(
+                    "|provider| error! failed to fs_read list provider resultfile (%s): %s, %s",
+                    vim.inspect(resultfile),
+                    vim.inspect(read_err),
+                    vim.inspect(read_name)
+                )
+                code = 130
+            elseif data_buffer then
+                -- foreach the data_buffer and find every line
+                local i = 1
+                while i <= #data_buffer do
+                    local newline_pos =
+                        shell_helpers.string_find(data_buffer, "\n", i)
+                    if not newline_pos then
+                        break
+                    end
+                    local line = data_buffer:sub(i, newline_pos)
+                    println(line)
+                    i = newline_pos + 1
+                end
+                if i <= #data_buffer then
+                    local line = data_buffer:sub(i, #data_buffer)
+                    println(line)
+                    data_buffer = nil
+                end
+            end
+            break
+        end
+
+        -- append data to data_buffer
+        data_buffer = data_buffer and (data_buffer .. data) or data --[[@as string]]
+        -- foreach the data_buffer and find every line
+        local i = 1
+        while i <= #data_buffer do
+            local newline_pos = shell_helpers.string_find(data_buffer, "\n", i)
+            if not newline_pos then
+                break
+            end
+            local line = data_buffer:sub(i, newline_pos)
+            println(line)
+            i = newline_pos + 1
+        end
+        -- truncate the printed lines if found any
+        data_buffer = i <= #data_buffer and data_buffer:sub(i, #data_buffer)
+            or nil
+
+        offset = offset + #data
+    end
+    vim.loop.fs_close(fd)
+    os.exit(code)
 
     -- local f = io.open(resultfile, "r")
     -- if f then
