ef1e8e9a5221934757713bda1821b3ffd38fa02a
linrongbin16 <linrongbin16@outlook.com>
2023-09-11 22:49:15 +0800

    fix: rtrim

--- lua/fzfx/shell_helpers.lua
+++ lua/fzfx/shell_helpers.lua
@@ -252,6 +252,8 @@ local M = {
     GitCurrentBranchCmd = require("fzfx.cmd").GitCurrentBranchCmd,
     string_find = require("fzfx.utils").string_find,
     string_rfind = require("fzfx.utils").string_rfind,
+    string_ltrim = require("fzfx.utils").string_ltrim,
+    string_rtrim = require("fzfx.utils").string_rtrim,
 }
 
 return M
