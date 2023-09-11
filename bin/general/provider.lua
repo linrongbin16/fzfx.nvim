e1598314b52ea86bf9b1bde39fd876da4e26f253
linrongbin16 <linrongbin16@outlook.com>
2023-09-11 21:20:33 +0800

    perf

--- bin/general/provider.lua
+++ bin/general/provider.lua
@@ -331,36 +331,6 @@ elseif metajson.provider_type == "list" then
     end
     vim.loop.fs_close(fd)
     os.exit(code)
-
-    -- local f = io.open(resultfile, "r")
-    -- if f then
-    --     if metajson.provider_line_type == "file" then
-    --         --- @diagnostic disable-next-line: need-check-nil
-    --         for line in f:lines("*line") do
-    --             if string.len(vim.fn.trim(line)) > 0 then
-    --                 local line_with_icon = shell_helpers.render_filepath_line(
-    --                     line,
-    --                     metajson.provider_line_delimiter,
-    --                     metajson.provider_line_pos
-    --                 )
-    --                 io.write(string.format("%s\n", line_with_icon))
-    --             end
-    --         end
-    --     else
-    --         for line in f:lines("*line") do
-    --             -- shell_helpers.log_debug("line:%s", vim.inspect(line))
-    --             if string.len(vim.fn.trim(line)) > 0 then
-    --                 io.write(string.format("%s\n", line))
-    --             end
-    --         end
-    --     end
-    --     f:close()
-    -- else
-    --     shell_helpers.debug(
-    --         "|provider| error! failed to open file on list provider resultfile! %s",
-    --         vim.inspect(resultfile)
-    --     )
-    -- end
 else
     shell_helpers.log_throw(
         "|provider| error! unknown provider type:%s",
