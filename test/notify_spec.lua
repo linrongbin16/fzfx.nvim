1e86570609dd69164a7edde535c8e777a33c74f5
linrongbin16 <linrongbin16@outlook.com>
2023-09-21 13:14:58 +0800

    test

--- test/notify_spec.lua
+++ test/notify_spec.lua
@@ -14,8 +14,24 @@ describe("notify", function()
 
     local notify = require("fzfx.notify")
     local NotifyLevels = require("fzfx.notify").NotifyLevels
+    local NotifyLevelNames = require("fzfx.notify").NotifyLevelNames
+    describe("[NotifyLevels]", function()
+        it("check levels", function()
+            for k, v in pairs(NotifyLevels) do
+                assert_eq(type(k), "string")
+                assert_eq(type(v), "number")
+            end
+        end)
+        it("check level names", function()
+            for v, k in pairs(NotifyLevelNames) do
+                assert_eq(type(k), "string")
+                assert_eq(type(v), "number")
+            end
+        end)
+    end)
+
     describe("[echo]", function()
-        it("echos", function()
+        it("info", function()
             notify.echo(NotifyLevels.INFO, "echo without parameters")
             notify.echo(NotifyLevels.INFO, "echo with 1 parameters: %s", "a")
             notify.echo(
@@ -26,6 +42,60 @@ describe("notify", function()
             )
             notify.echo(
                 NotifyLevels.INFO,
+                "echo with 3 parameters: %s, %d, %f",
+                "a",
+                1,
+                3.12
+            )
+            assert_true(true)
+        end)
+        it("debug", function()
+            notify.echo(NotifyLevels.DEBUG, "echo without parameters")
+            notify.echo(NotifyLevels.DEBUG, "echo with 1 parameters: %s", "a")
+            notify.echo(
+                NotifyLevels.DEBUG,
+                "echo with 2 parameters: %s, %d",
+                "a",
+                1
+            )
+            notify.echo(
+                NotifyLevels.DEBUG,
+                "echo with 3 parameters: %s, %d, %f",
+                "a",
+                1,
+                3.12
+            )
+            assert_true(true)
+        end)
+        it("warn", function()
+            notify.echo(NotifyLevels.WARN, "echo without parameters")
+            notify.echo(NotifyLevels.WARN, "echo with 1 parameters: %s", "a")
+            notify.echo(
+                NotifyLevels.WARN,
+                "echo with 2 parameters: %s, %d",
+                "a",
+                1
+            )
+            notify.echo(
+                NotifyLevels.WARN,
+                "echo with 3 parameters: %s, %d, %f",
+                "a",
+                1,
+                3.12
+            )
+            assert_true(true)
+        end)
+        it("err", function()
+            notify.echo(NotifyLevels.ERROR, "echo without parameters")
+            notify.echo(NotifyLevels.ERROR, "echo with 1 parameters: %s", "a")
+            notify.echo(
+                NotifyLevels.ERROR,
+                "echo with 2 parameters: %s, %d",
+                "a",
+                1
+            )
+            notify.echo(
+                NotifyLevels.ERROR,
                 "echo with 3 parameters: %s, %d, %f",
                 "a",
                 1,
