b98cc448e19f0a95b5a90cfd22e68eaf1b6db0d3
linrongbin16 <linrongbin16@outlook.com>
2023-09-21 10:46:38 +0800

    refactor: lua metatable (#190)
    * fix: test
    
    * refactor: lua __index
    
    * refactor: lua metatable
    
    * perf: remove extra opts

--- lua/fzfx/cmd.lua
+++ lua/fzfx/cmd.lua
@@ -4,19 +4,18 @@
 --- @field stdout string[]?
 --- @field stderr string[]?
 --- @field exitcode integer?
-local CmdResult = {
-    stdout = nil,
-    stderr = nil,
-    exitcode = nil,
-}
+local CmdResult = {}
 
 --- @return CmdResult
 function CmdResult:new()
-    return vim.tbl_deep_extend("force", vim.deepcopy(CmdResult), {
+    local o = {
         stdout = {},
         stderr = {},
         exitcode = nil,
-    })
+    }
+    setmetatable(o, self)
+    self.__index = self
+    return o
 end
 
 --- @return boolean
@@ -43,17 +42,13 @@ local Cmd = {
 --- @param opts table<string, any>?
 --- @return Cmd
 function Cmd:run(source, opts)
-    local c = vim.tbl_deep_extend("force", vim.deepcopy(Cmd), {
-        cmd = source,
-        jobid = nil,
-        result = CmdResult:new(),
-    })
+    local result = CmdResult:new()
 
     local function on_stdout(chanid, data, name)
         if type(data) == "table" then
             for _, d in ipairs(data) do
                 if type(d) == "string" and string.len(d) > 0 then
-                    table.insert(c.result.stdout, d)
+                    table.insert(result.stdout, d)
                 end
             end
         end
@@ -63,57 +58,34 @@ function Cmd:run(source, opts)
         if type(data) == "table" then
             for _, d in ipairs(data) do
                 if type(d) == "string" and string.len(d) > 0 then
-                    table.insert(c.result.stderr, d)
+                    table.insert(result.stderr, d)
                 end
             end
         end
     end
 
     local function on_exit(jobid2, exitcode, event)
-        c.result.exitcode = exitcode
+        result.exitcode = exitcode
     end
 
-    local on_stdout_opt = (
-        type(opts) == "table" and type(opts.on_stdout) == "function"
-    )
-            and opts.on_stdout
-        or on_stdout
-    local on_stderr_opt = (
-        type(opts) == "table" and type(opts.on_stderr) == "function"
-    )
-            and opts.on_stderr
-        or on_stderr
-
-    local on_exit_opt = (
-        type(opts) == "table" and type(opts.on_exit) == "function"
-    )
-            and opts.on_exit
-        or on_exit
-    local stdout_buffered_opt = (
-        type(opts) == "table"
-        and type(opts.stdout_buffered) == "boolean"
-        and opts.stdout_buffered
-    )
-            and true
-        or false
-    local stderr_buffered_opt = (
-        type(opts) == "table"
-        and type(opts.stderr_buffered) == "boolean"
-        and opts.stderr_buffered
-    )
-            and true
-        or false
-
     local jobid = vim.fn.jobstart(source, {
-        on_stdout = on_stdout_opt,
-        on_stderr = on_stderr_opt,
-        on_exit = on_exit_opt,
-        stdout_buffered = stdout_buffered_opt,
-        stderr_buffered = stderr_buffered_opt,
+        on_stdout = on_stdout,
+        on_stderr = on_stderr,
+        on_exit = on_exit,
+        stdout_buffered = true,
+        stderr_buffered = true,
     })
-    c.jobid = jobid
     vim.fn.jobwait({ jobid })
-    return c
+
+    local o = {
+        source = source,
+        jobid = jobid,
+        result = result,
+        opts = opts,
+    }
+    setmetatable(o, self)
+    self.__index = self
+    return o
 end
 
 --- @return boolean
@@ -123,16 +95,18 @@ end
 
 --- @class GitRootCmd
 --- @field result CmdResult?
-local GitRootCmd = {
-    result = nil,
-}
+local GitRootCmd = {}
 
 --- @return GitRootCmd
 function GitRootCmd:run()
     local cmd = Cmd:run({ "git", "rev-parse", "--show-toplevel" })
-    return vim.tbl_deep_extend("force", vim.deepcopy(GitRootCmd), {
+
+    local o = {
         result = cmd.result,
-    })
+    }
+    setmetatable(o, self)
+    self.__index = self
+    return o
 end
 
 --- @return boolean
@@ -152,18 +126,20 @@ end
 
 --- @class GitBranchCmd
 --- @field result CmdResult?
-local GitBranchCmd = {
-    result = nil,
-}
+local GitBranchCmd = {}
 
 --- @param remotes boolean?
 --- @return GitBranchCmd
 function GitBranchCmd:run(remotes)
     local cmd = remotes and Cmd:run({ "git", "branch", "--remotes" })
         or Cmd:run({ "git", "branch" })
-    return vim.tbl_deep_extend("force", vim.deepcopy(GitBranchCmd), {
+
+    local o = {
         result = cmd.result,
-    })
+    }
+    setmetatable(o, self)
+    self.__index = self
+    return o
 end
 
 --- @return boolean
@@ -186,9 +162,9 @@ function GitBranchCmd:current_branch()
     end
     if type(self.result.stdout) == "table" and #self.result.stdout > 0 then
         for _, out in ipairs(self.result.stdout) do
-            local trim_out = vim.trim(out)
-            if string.len(trim_out) > 0 and trim_out[1] == "*" then
-                return trim_out
+            local line = vim.trim(out)
+            if string.len(line) > 0 and line[1] == "*" then
+                return line
             end
         end
     end
@@ -197,17 +173,19 @@ end
 
 --- @class GitCurrentBranchCmd
 --- @field result CmdResult?
-local GitCurrentBranchCmd = {
-    result = nil,
-}
+local GitCurrentBranchCmd = {}
 
 --- @return GitCurrentBranchCmd
 function GitCurrentBranchCmd:run()
     -- git rev-parse --abbrev-ref HEAD
     local cmd = Cmd:run({ "git", "rev-parse", "--abbrev-ref", "HEAD" })
-    return vim.tbl_deep_extend("force", vim.deepcopy(GitCurrentBranchCmd), {
+
+    local o = {
         result = cmd.result,
-    })
+    }
+    setmetatable(o, self)
+    self.__index = self
+    return o
 end
 
 --- @return boolean
