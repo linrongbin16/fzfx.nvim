db0a057077f0ca47360f336bfee3eb37b3e53b4c
linrongbin16 <linrongbin16@outlook.com>
2023-09-26 14:02:44 +0800

    test

---
+++ test/shell_helpers_spec.lua
@@ -0,0 +1,54 @@
+local cwd = vim.fn.getcwd()
+
+describe("shell_helpers", function()
+    local assert_eq = assert.is_equal
+    local assert_true = assert.is_true
+    local assert_false = assert.is_false
+
+    before_each(function()
+        vim.api.nvim_command("cd " .. cwd)
+    end)
+
+    local shell_helpers = require("fzfx.shell_helpers")
+    describe("[is_windows]", function()
+        it("is windows", function()
+            assert_eq(type(shell_helpers.is_windows), "boolean")
+        end)
+    end)
+    describe("[log]", function()
+        it("debug", function()
+            assert_true(shell_helpers.log_debug("logs without params") == nil)
+            assert_true(
+                shell_helpers.log_debug("logs with params 1, %d", 1) == nil
+            )
+            assert_true(
+                shell_helpers.log_debug("logs with params 2, %d, %s", 1, "asdf")
+                    == nil
+            )
+        end)
+        it("err", function()
+            assert_true(shell_helpers.log_err("logs without params") == nil)
+            assert_true(
+                shell_helpers.log_err("logs with params 1, %d", 1) == nil
+            )
+            assert_true(
+                shell_helpers.log_err("logs with params 2, %d, %s", 1, "asdf")
+                    == nil
+            )
+        end)
+        it("ensure", function()
+            assert_true(
+                shell_helpers.log_ensure(true, "logs without params") == nil
+            )
+            local ok, err = pcall(
+                shell_helpers.log_ensure,
+                false,
+                "logs with params 1, %d",
+                1
+            )
+            assert_false(ok)
+            assert_eq(type(err), "string")
+            assert_true(string.len(err --[[@as string]]) > 0)
+        end)
+    end)
+end)
