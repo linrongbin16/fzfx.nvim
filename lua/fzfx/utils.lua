9523f9a8bb6bd7b57788b8d222b3f04ea4e5041f
linrongbin16 <linrongbin16@outlook.com>
2023-10-06 23:42:57 +0800

    fix: keep legacy API for 0.8

--- lua/fzfx/utils.lua
+++ lua/fzfx/utils.lua
@@ -6,7 +6,7 @@ local constants = require("fzfx.constants")
 --- @param name string
 --- @return any
 local function get_buf_option(bufnr, name)
-    if vim.fn.has("nvim-0.7") > 0 then
+    if vim.fn.has("nvim-0.8") > 0 then
         return vim.api.nvim_get_option_value(name, { buf = bufnr })
     else
         return vim.api.nvim_buf_get_option(bufnr, name)
@@ -18,7 +18,7 @@ end
 --- @param value any
 --- @return any
 local function set_buf_option(bufnr, name, value)
-    if vim.fn.has("nvim-0.7") > 0 then
+    if vim.fn.has("nvim-0.8") > 0 then
         return vim.api.nvim_set_option_value(name, value, { buf = bufnr })
     else
         return vim.api.nvim_buf_set_option(bufnr, name, value)
@@ -42,7 +42,7 @@ end
 --- @param name string
 --- @return any
 local function get_win_option(winnr, name)
-    if vim.fn.has("nvim-0.7") > 0 then
+    if vim.fn.has("nvim-0.8") > 0 then
         return vim.api.nvim_get_option_value(name, { win = winnr })
     else
         return vim.api.nvim_win_get_option(winnr, name)
@@ -54,7 +54,7 @@ end
 --- @param value any
 --- @return any
 local function set_win_option(winnr, name, value)
-    if vim.fn.has("nvim-0.7") > 0 then
+    if vim.fn.has("nvim-0.8") > 0 then
         return vim.api.nvim_set_option_value(name, value, { win = winnr })
     else
         return vim.api.nvim_win_set_option(winnr, name, value)
