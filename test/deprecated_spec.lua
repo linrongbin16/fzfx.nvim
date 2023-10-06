b550d22853e72473314478c1212c3745c25521e0
linrongbin16 <linrongbin16@outlook.com>
2023-10-06 23:23:37 +0800

    test: deprecated

---
+++ test/deprecated_spec.lua
@@ -0,0 +1,28 @@
+local cwd = vim.fn.getcwd()
+
+describe("deprecated", function()
+    local assert_eq = assert.is_equal
+    local assert_true = assert.is_true
+    local assert_false = assert.is_false
+
+    before_each(function()
+        vim.api.nvim_command("cd " .. cwd)
+    end)
+
+    local deprecated = require("fzfx.deprecated")
+    describe("[notify]", function()
+        it("notify without parameters", function()
+            deprecated.notify("notify without parameters")
+            assert_true(true)
+        end)
+        it("notify with parameters", function()
+            deprecated.notify(
+                "notify with parameters: %s, %d, %f",
+                "asdf",
+                2,
+                3.14
+            )
+            assert_true(true)
+        end)
+    end)
+end)
