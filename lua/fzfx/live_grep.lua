1c15aa9d8c774a4ed9fc3926d1a701ac5c1a38b0
linrongbin16 <linrongbin16@outlook.com>
2023-10-07 14:24:08 +0800

    perf: use deprecated notify

--- lua/fzfx/live_grep.lua
+++ lua/fzfx/live_grep.lua
@@ -64,20 +64,9 @@ local function setup()
     end
     general.setup("live_grep", live_grep_configs)
     if deprecated then
-        local function deprecated_notification()
-            log.echo(
-                LogLevels.WARN,
-                "deprecated 'FzfxLiveGrep' configs, please migrate to latest config schema!"
-            )
-        end
-        local delay = 3 * 1000
-        vim.defer_fn(deprecated_notification, delay)
-        vim.api.nvim_create_autocmd("VimEnter", {
-            pattern = { "*" },
-            callback = function()
-                vim.defer_fn(deprecated_notification, delay)
-            end,
-        })
+        require("fzfx.deprecated").notify(
+            "deprecated 'FzfxLiveGrep' configs, please migrate to latest config schema!"
+        )
     end
 end
 
