64e46fc092fd452b2d61d679e8b40fa2fd485d76
linrongbin16 <linrongbin16@outlook.com>
2023-10-11 13:11:34 +0800

    refactor(line_helpers): remove dead code

--- lua/fzfx/line_helpers.lua
+++ lua/fzfx/line_helpers.lua
@@ -126,78 +126,6 @@ end
 local parse_ls = make_parse_ls(8)
 local parse_eza = constants.is_windows and make_parse_ls(5) or make_parse_ls(6)
 
---- @param line string
---- @return string
-local function parse_filename(line)
-    local filename = nil
-    if env.icon_enable() then
-        local splits = utils.string_split(line, " ")
-        filename = splits[#splits]
-    else
-        filename = line
-    end
-    return path.normalize(filename)
-end
-
---- @alias PathLineParsedResult {filename:string,lineno:string?,column:string?}
---- @param line string
---- @param delimiter string?
---- @param file_pos integer?
---- @param lineno_pos integer?
---- @param colno_pos integer?
---- @return PathLineParsedResult
-local function parse_path_line(line, delimiter, file_pos, lineno_pos, colno_pos)
-    local filename = nil
-    local lineno = nil
-    local column = nil
-    if type(delimiter) == "string" and string.len(delimiter) > 0 then
-        local parts = utils.string_split(line, delimiter)
-        filename = parse_filename(
-            parts[file_pos > 0 and file_pos or (#parts + file_pos + 1)]
-        )
-        if type(lineno_pos) == "number" then
-            lineno = tonumber(
-                parts[lineno_pos > 0 and lineno_pos or (#parts + lineno_pos + 1)]
-            )
-        end
-        if type(colno_pos) == "number" then
-            column = tonumber(
-                parts[colno_pos > 0 and colno_pos or (#parts + colno_pos + 1)]
-            )
-        end
-    else
-        filename = parse_filename(line)
-    end
-    return { filename = filename, lineno = lineno, column = column }
-end
-
---- @class PathLine
---- @field source string
---- @field filename string
---- @field lineno integer?
---- @field column integer?
-local PathLine = {}
-
---- @param line string
---- @param delimiter string?
---- @param file_pos integer?
---- @param lineno_pos integer?
---- @param colno_pos integer?
---- @return PathLine
-function PathLine:new(line, delimiter, file_pos, lineno_pos, colno_pos)
-    local parsed =
-        parse_path_line(line, delimiter, file_pos, lineno_pos, colno_pos)
-    local o = {
-        line = line,
-        filename = parsed.filename,
-        lineno = parsed.lineno,
-        column = parsed.column,
-    }
-    setmetatable(o, self)
-    self.__index = self
-    return o
-end
-
 local M = {
     parse_find = parse_find,
     parse_grep = parse_grep,
@@ -205,9 +133,6 @@ local M = {
     make_parse_ls = make_parse_ls,
     parse_ls = parse_ls,
     parse_eza = parse_eza,
-    parse_filename = parse_filename,
-    parse_path_line = parse_path_line,
-    PathLine = PathLine,
 }
 
 return M
