341c5c1e7186337442dc425f8468b8f96b769379
linrongbin16 <linrongbin16@outlook.com>
2023-10-30 11:36:46 +0800

    chore

--- test/spawn_spec.lua
+++ test/spawn_spec.lua
@@ -14,17 +14,14 @@ describe("utils", function()
 
     describe("[Spawn]", function()
         it("open", function()
-            local acmd = spawn.Spawn:make(
-                { "cat", "README.md" },
-                function() end
-            ) --[[@as Spawn]]
-            assert_eq(type(acmd), "table")
-            assert_eq(type(acmd.cmds), "table")
-            assert_eq(#acmd.cmds, 2)
-            assert_eq(acmd.cmds[1], "cat")
-            assert_eq(acmd.cmds[2], "README.md")
-            assert_eq(type(acmd.out_pipe), "userdata")
-            assert_eq(type(acmd.err_pipe), "userdata")
+            local sp = spawn.Spawn:make({ "cat", "README.md" }, function() end) --[[@as Spawn]]
+            assert_eq(type(sp), "table")
+            assert_eq(type(sp.cmds), "table")
+            assert_eq(#sp.cmds, 2)
+            assert_eq(sp.cmds[1], "cat")
+            assert_eq(sp.cmds[2], "README.md")
+            assert_eq(type(sp.out_pipe), "userdata")
+            assert_eq(type(sp.err_pipe), "userdata")
         end)
         it("consume line", function()
             local content = utils.readfile("README.md") --[[@as string]]
@@ -37,8 +34,8 @@ describe("utils", function()
                 assert_eq(line, lines[i])
                 i = i + 1
             end
-            local acmd = spawn.Spawn:make({ "cat", "README.md" }, process_line) --[[@as Spawn]]
-            local pos = acmd:_consume_line(content, process_line)
+            local sp = spawn.Spawn:make({ "cat", "README.md" }, process_line) --[[@as Spawn]]
+            local pos = sp:_consume_line(content, process_line)
             if pos <= #content then
                 local line = content:sub(pos, #content)
                 process_line(line)
@@ -55,17 +52,17 @@ describe("utils", function()
                 assert_eq(line, lines[i])
                 i = i + 1
             end
-            local acmd = spawn.Spawn:make({ "cat", "README.md" }, process_line) --[[@as Spawn]]
+            local sp = spawn.Spawn:make({ "cat", "README.md" }, process_line) --[[@as Spawn]]
             local content_splits =
                 utils.string_split(content, "\n", { trimempty = false })
             for j, splits in ipairs(content_splits) do
-                acmd:_on_stdout(nil, splits)
+                sp:_on_stdout(nil, splits)
                 if j < #content_splits then
-                    acmd:_on_stdout(nil, "\n")
+                    sp:_on_stdout(nil, "\n")
                 end
             end
-            acmd:_on_stdout(nil, nil)
-            assert_true(acmd.out_pipe:is_closing())
+            sp:_on_stdout(nil, nil)
+            assert_true(sp.out_pipe:is_closing())
         end)
         it("stdout on whitespace", function()
             local content = utils.readfile("README.md") --[[@as string]]
@@ -78,17 +75,17 @@ describe("utils", function()
                 assert_eq(line, lines[i])
                 i = i + 1
             end
-            local acmd = spawn.Spawn:make({ "cat", "README.md" }, process_line) --[[@as Spawn]]
+            local sp = spawn.Spawn:make({ "cat", "README.md" }, process_line) --[[@as Spawn]]
             local content_splits =
                 utils.string_split(content, " ", { trimempty = false })
             for j, splits in ipairs(content_splits) do
-                acmd:_on_stdout(nil, splits)
+                sp:_on_stdout(nil, splits)
                 if j < #content_splits then
-                    acmd:_on_stdout(nil, " ")
+                    sp:_on_stdout(nil, " ")
                 end
             end
-            acmd:_on_stdout(nil, nil)
-            assert_true(acmd.out_pipe:is_closing())
+            sp:_on_stdout(nil, nil)
+            assert_true(sp.out_pipe:is_closing())
         end)
         local delimiter_i = 0
         while delimiter_i <= 25 do
@@ -105,7 +102,7 @@ describe("utils", function()
                     assert_eq(line, lines[i])
                     i = i + 1
                 end
-                local acmd =
+                local sp =
                     spawn.Spawn:make({ "cat", "README.md" }, process_line) --[[@as Spawn]]
                 local content_splits = utils.string_split(
                     content,
@@ -113,13 +110,13 @@ describe("utils", function()
                     { trimempty = false }
                 )
                 for j, splits in ipairs(content_splits) do
-                    acmd:_on_stdout(nil, splits)
+                    sp:_on_stdout(nil, splits)
                     if j < #content_splits then
-                        acmd:_on_stdout(nil, lower_char)
+                        sp:_on_stdout(nil, lower_char)
                     end
                 end
-                acmd:_on_stdout(nil, nil)
-                assert_true(acmd.out_pipe:is_closing())
+                sp:_on_stdout(nil, nil)
+                assert_true(sp.out_pipe:is_closing())
             end)
             -- upper case: A
             local upper_char = string.char(65 + delimiter_i)
@@ -134,7 +131,7 @@ describe("utils", function()
                     assert_eq(line, lines[i])
                     i = i + 1
                 end
-                local acmd =
+                local sp =
                     spawn.Spawn:make({ "cat", "README.md" }, process_line) --[[@as Spawn]]
                 local content_splits = utils.string_split(
                     content,
@@ -142,23 +139,32 @@ describe("utils", function()
                     { trimempty = false }
                 )
                 for j, splits in ipairs(content_splits) do
-                    acmd:_on_stdout(nil, splits)
+                    sp:_on_stdout(nil, splits)
                     if j < #content_splits then
-                        acmd:_on_stdout(nil, upper_char)
+                        sp:_on_stdout(nil, upper_char)
                     end
                 end
-                acmd:_on_stdout(nil, nil)
-                assert_true(acmd.out_pipe:is_closing())
+                sp:_on_stdout(nil, nil)
+                assert_true(sp.out_pipe:is_closing())
             end)
             delimiter_i = delimiter_i + math.random(1, 5)
         end
         it("stderr", function()
-            local acmd = spawn.Spawn:make(
-                { "cat", "README.md" },
-                function() end
+            local sp = spawn.Spawn:make({ "cat", "README.md" }, function() end) --[[@as Spawn]]
+            sp:_on_stderr(nil, nil)
+            assert_true(sp.err_pipe:is_closing())
+        end)
+        it("stderr2", function()
+            local i = 1
+            local function process_line(line)
+                print(string.format("process[%d]:%s\n", i, line))
+            end
+            local sp = spawn.Spawn:make(
+                { "cat", "non_exists.txt" },
+                process_line,
+                process_line
             ) --[[@as Spawn]]
-            acmd:_on_stderr(nil, nil)
-            assert_true(acmd.err_pipe:is_closing())
+            sp:run()
         end)
         it("iterate on README.md", function()
             local lines = utils.readlines("README.md") --[[@as table]]
@@ -171,8 +177,8 @@ describe("utils", function()
                 i = i + 1
             end
 
-            local acmd = spawn.Spawn:make({ "cat", "README.md" }, process_line) --[[@as Spawn]]
-            acmd:run()
+            local sp = spawn.Spawn:make({ "cat", "README.md" }, process_line) --[[@as Spawn]]
+            sp:run()
         end)
         it("iterate on lua/fzfx/config.lua", function()
             local lines = utils.readlines("lua/fzfx/config.lua") --[[@as table]]
@@ -185,19 +191,19 @@ describe("utils", function()
                 i = i + 1
             end
 
-            local acmd =
+            local sp =
                 spawn.Spawn:make({ "cat", "lua/fzfx/config.lua" }, process_line) --[[@as Spawn]]
-            acmd:run()
+            sp:run()
         end)
         it("close handle", function()
-            local acmd = spawn.Spawn:make(
+            local sp = spawn.Spawn:make(
                 { "cat", "lua/fzfx/config.lua" },
                 function() end
             ) --[[@as Spawn]]
-            acmd:run()
-            assert_true(acmd.process_handle ~= nil)
-            acmd:_close_handle(acmd.process_handle)
-            assert_true(acmd.process_handle:is_closing())
+            sp:run()
+            assert_true(sp.process_handle ~= nil)
+            sp:_close_handle(sp.process_handle)
+            assert_true(sp.process_handle:is_closing())
         end)
     end)
 end)
