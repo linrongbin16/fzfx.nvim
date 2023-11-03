f5e19d9e65fa2336555bc9a8ed06334d3860c818
linrongbin16 <linrongbin16@outlook.com>
2023-11-03 12:43:02 +0800

    chore

--- lua/fzfx/ui.lua
+++ lua/fzfx/ui.lua
@@ -2,27 +2,26 @@ local log = require("fzfx.log")
 local LogLevels = require("fzfx.log").LogLevels
 local utils = require("fzfx.utils")
 
-local default_user_canceled_error = "canceled."
+local user_canceled_error = "canceled."
 
 --- @param bufnr integer
 --- @param callback fun():any
 --- @return boolean
 local function confirm_discard_buffer_modified(bufnr, callback)
     if utils.get_buf_option(bufnr, "modified") then
-        vim.ui.input(
-            { prompt = "Current buffer has unsaved changes, discard? (y/n)" },
-            function(input)
-                if
-                    type(input) == "string"
-                    and string.len(input) > 0
-                    and utils.string_startswith(input:lower(), "y")
-                then
-                    callback()
-                else
-                    log.echo(LogLevels.INFO, default_user_canceled_error)
-                end
+        vim.ui.input({
+            prompt = "[fzfx] current buffer has been modified, continue? (y/n) ",
+        }, function(input)
+            if
+                type(input) == "string"
+                and string.len(input) > 0
+                and utils.string_startswith(input:lower(), "y")
+            then
+                callback()
+            else
+                log.echo(LogLevels.INFO, user_canceled_error)
             end
-        )
+        end)
     else
         callback()
     end
