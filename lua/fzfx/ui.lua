0e4ecc8f9e105aa8d5709e133e0a614fa493cd63
linrongbin16 <linrongbin16@outlook.com>
2023-11-04 23:02:18 +0800

    fix(ui): fallback to vim.fn.input to avoid issues

--- lua/fzfx/ui.lua
+++ lua/fzfx/ui.lua
@@ -8,22 +8,18 @@ local user_cancelled_error = "cancelled."
 --- @param callback fun():any
 local function confirm_discard_buffer_modified(bufnr, callback)
     if utils.get_buf_option(bufnr, "modified") then
-        vim.ui.input({
+        local user_input = vim.fn.input({
             prompt = "[fzfx] current buffer has been modified, continue? (y/n) ",
-        }, function(input)
-            if
-                type(input) == "string"
-                and string.len(input) > 0
-                and utils.string_startswith(input:lower(), "y")
-            then
-                callback()
-            else
-                log.echo(LogLevels.INFO, user_cancelled_error)
-            end
-        end)
-        local current_mode = vim.api.nvim_get_mode()
-        if utils.string_find(current_mode.mode, "i") == nil then
-            vim.api.nvim_feedkeys("i", "m", false)
+            cancelreturn = "n",
+        })
+        if
+            type(user_input) == "string"
+            and string.len(user_input) > 0
+            and utils.string_startswith(user_input:lower(), "y")
+        then
+            callback()
+        else
+            log.echo(LogLevels.INFO, user_cancelled_error)
         end
     else
         callback()
