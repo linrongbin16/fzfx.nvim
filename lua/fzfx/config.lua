a543ca41158156e32b82dc8a2f2a17696231c9c5
linrongbin16 <linrongbin16@outlook.com>
2023-11-08 14:25:05 +0800

    chore

--- lua/fzfx/config.lua
+++ lua/fzfx/config.lua
@@ -4050,7 +4050,7 @@ local Defaults = {
         marker = { "fg", "Keyword" },
         spinner = { "fg", "Label" },
         header = { "fg", "Comment" },
-        preview_label = { "fg", "PreProc" },
+        preview_label = { "fg", "Label" },
     },
 
     -- icons
