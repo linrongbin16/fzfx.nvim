333c84d6bab0cf5d23a225f666c0c6be9bf09a7a
linrongbin16 <linrongbin16@outlook.com>
2023-11-08 19:20:36 +0800

    chore

--- test/previewer_labels_spec.lua
+++ test/previewer_labels_spec.lua
@@ -11,7 +11,7 @@ describe("previewer_labels", function()
 
     local utils = require("fzfx.utils")
     local previewer_labels = require("fzfx.previewer_labels")
-    describe("[_make_find_previewer_label]", function()
+    describe("[_make_find_previewer_label/find_previewer_label]", function()
         it("makes", function()
             vim.env._FZFX_NVIM_DEVICONS_PATH = nil
             local lines = {
@@ -23,26 +23,69 @@ describe("previewer_labels", function()
             }
             for _, line in ipairs(lines) do
                 local f = previewer_labels._make_find_previewer_label(line)
-                local actual = f(line)
-                assert_eq(type(actual), "string")
-                assert_true(utils.string_endswith(line, actual))
+                local actual1 = f(line)
+                assert_eq(type(actual1), "string")
+                assert_true(utils.string_endswith(line, actual1))
+                local actual2 = previewer_labels.find_previewer_label(line)
+                assert_eq(type(actual2), "string")
+                assert_true(utils.string_endswith(line, actual2))
+                assert_eq(actual1, actual2)
             end
         end)
     end)
-    describe("[find_previewer_label]", function()
+    describe("[_make_rg_previewer_label/rg_previewer_label]", function()
         it("makes", function()
             vim.env._FZFX_NVIM_DEVICONS_PATH = nil
             local lines = {
-                "~/github/linrongbin16/fzfx.nvim/README.md",
-                "~/github/linrongbin16/fzfx.nvim/lua/fzfx.lua",
-                "~/github/linrongbin16/fzfx.nvim/lua/fzfx/config.lua",
-                "~/github/linrongbin16/fzfx.nvim/lua/fzfx/test/goodbye world/goodbye.lua",
-                "~/github/linrongbin16/fzfx.nvim/lua/fzfx/test/hello world.txt",
+                "~/github/linrongbin16/fzfx.nvim/README.md:1:1:ok",
+                "~/github/linrongbin16/fzfx.nvim/lua/fzfx.lua:1:2:hello",
+                "~/github/linrongbin16/fzfx.nvim/lua/fzfx/config.lua:1:3:hello world",
+                "~/github/linrongbin16/fzfx.nvim/lua/fzfx/test/goodbye world/goodbye.lua:12:81: goodbye",
+                "~/github/linrongbin16/fzfx.nvim/lua/fzfx/test/hello world.txt:81:71:9129",
             }
             for _, line in ipairs(lines) do
-                local actual = previewer_labels.find_previewer_label(line)
-                assert_eq(type(actual), "string")
-                assert_true(utils.string_endswith(line, actual))
+                local f = previewer_labels._make_rg_previewer_label(line)
+                local actual1 = f(line)
+                assert_eq(type(actual1), "string")
+                assert_eq(type(utils.string_find(line, actual1)), "number")
+                assert_true(utils.string_find(line, actual1) > 0)
+                local actual2 = previewer_labels.rg_previewer_label(line)
+                assert_eq(type(actual2), "string")
+                assert_eq(type(utils.string_find(line, actual2)), "number")
+                assert_true(utils.string_find(line, actual2) > 0)
+                assert_eq(actual1, actual2)
+                assert_eq(
+                    utils.string_find(line, actual1),
+                    utils.string_find(line, actual2)
+                )
+            end
+        end)
+    end)
+    describe("[_make_grep_previewer_label/grep_previewer_label]", function()
+        it("makes", function()
+            vim.env._FZFX_NVIM_DEVICONS_PATH = nil
+            local lines = {
+                "~/github/linrongbin16/fzfx.nvim/README.md:73",
+                "~/github/linrongbin16/fzfx.nvim/lua/fzfx.lua:1",
+                "~/github/linrongbin16/fzfx.nvim/lua/fzfx/config.lua:1:hello world",
+                "~/github/linrongbin16/fzfx.nvim/lua/fzfx/test/goodbye world/goodbye.lua:12:81: goodbye",
+                "~/github/linrongbin16/fzfx.nvim/lua/fzfx/test/hello world.txt:81:72:9129",
+            }
+            for _, line in ipairs(lines) do
+                local f = previewer_labels._make_grep_previewer_label(line)
+                local actual1 = f(line)
+                assert_eq(type(actual1), "string")
+                assert_eq(type(utils.string_find(line, actual1)), "number")
+                assert_true(utils.string_find(line, actual1) > 0)
+                local actual2 = previewer_labels.grep_previewer_label(line)
+                assert_eq(type(actual2), "string")
+                assert_eq(type(utils.string_find(line, actual2)), "number")
+                assert_true(utils.string_find(line, actual2) > 0)
+                assert_eq(actual1, actual2)
+                assert_eq(
+                    utils.string_find(line, actual1),
+                    utils.string_find(line, actual2)
+                )
             end
         end)
     end)
