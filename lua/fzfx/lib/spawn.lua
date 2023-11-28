d18e39f143eea4d6689643a551cfd659ed91febd
linrongbin16 <linrongbin16@outlook.com>
2023-11-28 22:29:25 +0800

    chore

--- lua/fzfx/lib/spawn.lua
+++ lua/fzfx/lib/spawn.lua
@@ -5,11 +5,16 @@ local uv = (vim.fn.has("nvim-0.10") > 0 and vim.uv ~= nil) and vim.uv
 
 local M = {}
 
---- @alias fzfx.SpawnLineConsumer fun(line:string):any
---- @class fzfx._Spawn
+--- @alias fzfx.SpawnLineProcessor fun(line:string):any
+--- @alias fzfx.SpawnOnStdout fzfx.SpawnLineProcessor
+--- @alias fzfx.SpawnOnStderr fzfx.SpawnLineProcessor
+--- @alias fzfx.SpawnOnExit fun(code:integer?,signal:integer?):any
+--- @class fzfx.Spawn
 --- @field cmds string[]
---- @field fn_out_line_consumer fzfx.SpawnLineConsumer
---- @field fn_err_line_consumer fzfx.SpawnLineConsumer
+--- @field fn_on_exit fzfx.SpawnOnExit
+--- @field result {code:integer?,signal:integer?}?
+--- @field fn_on_stdout fzfx.SpawnOnStdout
+--- @field fn_on_stderr fzfx.SpawnOnStderr
 --- @field out_pipe uv_pipe_t
 --- @field err_pipe uv_pipe_t
 --- @field out_buffer string?
@@ -17,26 +22,30 @@ local M = {}
 --- @field process_handle uv_process_t?
 --- @field process_id integer|string|nil
 --- @field _close_count integer
---- @field result {code:integer?,signal:integer?}?
 --- @field _blocking boolean
-local _Spawn = {}
+local Spawn = {}
 
 --- @param cmds string[]
---- @param fn_out_line_consumer fzfx.SpawnLineConsumer
---- @param fn_err_line_consumer fzfx.SpawnLineConsumer
---- @param blocking boolean?
---- @return fzfx._Spawn?
-function _Spawn:make(cmds, fn_out_line_consumer, fn_err_line_consumer, blocking)
+--- @param opts {on_stdout:fzfx.SpawnOnStdout,on_stderr:fzfx.SpawnOnStderr?,on_exit:fzfx.SpawnOnExit?,blocking:boolean}
+--- @return fzfx.Spawn
+function Spawn:make(cmds, opts)
+  assert(type(opts) == "table")
+  assert(type(opts.on_stdout) == "function")
+  assert(type(opts.on_stderr) == "function" or opts.on_stderr == nil)
+  assert(type(opts.on_exit) == "function" or opts.on_exit == nil)
+  assert(type(opts.blocking) == "boolean")
+
   local out_pipe = uv.new_pipe(false) --[[@as uv_pipe_t]]
   local err_pipe = uv.new_pipe(false) --[[@as uv_pipe_t]]
-  if not out_pipe or not err_pipe then
-    return nil
-  end
+  assert(out_pipe ~= nil)
+  assert(err_pipe ~= nil)
 
   local o = {
     cmds = cmds,
-    fn_out_line_consumer = fn_out_line_consumer,
-    fn_err_line_consumer = fn_err_line_consumer,
+    fn_on_exit = opts.on_exit,
+    result = nil,
+    fn_on_stdout = opts.on_stdout,
+    fn_on_stderr = opts.on_stderr or function() end,
     out_pipe = out_pipe,
     err_pipe = err_pipe,
     out_buffer = nil,
@@ -44,8 +53,7 @@ function _Spawn:make(cmds, fn_out_line_consumer, fn_err_line_consumer, blocking)
     process_handle = nil,
     process_id = nil,
     _close_count = 0,
-    result = nil,
-    _blocking = type(blocking) == "boolean" and blocking or true,
+    _blocking = opts.blocking,
   }
   setmetatable(o, self)
   self.__index = self
@@ -53,9 +61,9 @@ function _Spawn:make(cmds, fn_out_line_consumer, fn_err_line_consumer, blocking)
 end
 
 --- @param buffer string
---- @param fn_line_processor fzfx.SpawnLineConsumer
+--- @param fn_line_processor fzfx.SpawnLineProcessor
 --- @return integer
-function _Spawn:_consume_line(buffer, fn_line_processor)
+function Spawn:_consume_line(buffer, fn_line_processor)
   local i = 1
   while i <= #buffer do
     local newline_pos = strs.find(buffer, "\n", i)
@@ -70,12 +78,17 @@ function _Spawn:_consume_line(buffer, fn_line_processor)
 end
 
 --- @param handle uv_handle_t
-function _Spawn:_close_handle(handle)
+--- @param code integer?
+--- @param signal integer?
+function Spawn:_close_handle(handle, code, signal)
   if handle and not handle:is_closing() then
     handle:close(function()
       self._close_count = self._close_count + 1
       if self._blocking and self._close_count >= 3 then
         uv.stop()
+      end
+      if type(self.fn_on_exit) == "function" then
+        self.fn_on_exit(code, signal)
       end
     end)
   end
@@ -83,7 +96,7 @@ end
 
 --- @param err string?
 --- @param data string?
-function _Spawn:_on_stdout(err, data)
+function Spawn:_on_stdout(err, data)
   if err then
     self.out_pipe:read_stop()
     self:_close_handle(self.out_pipe)
@@ -95,7 +108,7 @@ function _Spawn:_on_stdout(err, data)
     self.out_buffer = self.out_buffer and (self.out_buffer .. data) or data
     self.out_buffer = self.out_buffer:gsub("\r\n", "\n")
     -- foreach the data_buffer and find every line
-    local i = self:_consume_line(self.out_buffer, self.fn_out_line_consumer)
+    local i = self:_consume_line(self.out_buffer, self.fn_on_stdout)
     -- truncate the printed lines if found any
     self.out_buffer = i <= #self.out_buffer
         and self.out_buffer:sub(i, #self.out_buffer)
@@ -103,10 +116,10 @@ function _Spawn:_on_stdout(err, data)
   else
     if self.out_buffer then
       -- foreach the data_buffer and find every line
-      local i = self:_consume_line(self.out_buffer, self.fn_out_line_consumer)
+      local i = self:_consume_line(self.out_buffer, self.fn_on_stdout)
       if i <= #self.out_buffer then
         local line = self.out_buffer:sub(i, #self.out_buffer)
-        self.fn_out_line_consumer(line)
+        self.fn_on_stdout(line)
         self.out_buffer = nil
       end
     end
@@ -117,7 +130,7 @@ end
 
 --- @param err string?
 --- @param data string?
-function _Spawn:_on_stderr(err, data)
+function Spawn:_on_stderr(err, data)
   if err then
     io.write(
       string.format(
@@ -143,7 +156,7 @@ function _Spawn:_on_stderr(err, data)
     self.err_buffer = self.err_buffer and (self.err_buffer .. data) or data
     self.err_buffer = self.err_buffer:gsub("\r\n", "\n")
     -- foreach the data_buffer and find every line
-    local i = self:_consume_line(self.err_buffer, self.fn_err_line_consumer)
+    local i = self:_consume_line(self.err_buffer, self.fn_on_stderr)
     -- truncate the printed lines if found any
     self.err_buffer = i <= #self.err_buffer
         and self.err_buffer:sub(i, #self.err_buffer)
@@ -151,10 +164,10 @@ function _Spawn:_on_stderr(err, data)
   else
     if self.err_buffer then
       -- foreach the data_buffer and find every line
-      local i = self:_consume_line(self.err_buffer, self.fn_err_line_consumer)
+      local i = self:_consume_line(self.err_buffer, self.fn_on_stderr)
       if i <= #self.err_buffer then
         local line = self.err_buffer:sub(i, #self.err_buffer)
-        self.fn_err_line_consumer(line)
+        self.fn_on_stderr(line)
         self.err_buffer = nil
       end
     end
@@ -163,14 +176,14 @@ function _Spawn:_on_stderr(err, data)
   end
 end
 
-function _Spawn:run()
+function Spawn:run()
   self.process_handle, self.process_id = uv.spawn(self.cmds[1], {
     args = vim.list_slice(self.cmds, 2),
     stdio = { nil, self.out_pipe, self.err_pipe },
     hide = true,
   }, function(code, signal)
     self.result = { code = code, signal = signal }
-    self:_close_handle(self.process_handle)
+    self:_close_handle(self.process_handle, code, signal)
   end)
 
   self.out_pipe:read_start(function(err, data)
@@ -187,36 +200,6 @@ function _Spawn:run()
   end
 end
 
-M._Spawn = _Spawn
-
---- @param cmds string[]
---- @param opts {on_stdout:fzfx.SpawnLineConsumer, on_stderr:fzfx.SpawnLineConsumer?, blocking:boolean}
-M.spawn = function(cmds, opts)
-  assert(type(opts) == "table")
-  assert(type(opts.blocking) == "boolean")
-  assert(type(opts.on_stdout) == "function")
-  assert(type(opts.on_stderr) == "function" or opts.on_stderr == nil)
-  return M._Spawn
-    :make(cmds, opts.on_stdout, opts.on_stderr or function() end, opts.blocking)
-    :run()
-end
-
---- @param cmds string[]
---- @param opts {on_stdout:fzfx.SpawnLineConsumer, on_stderr:fzfx.SpawnLineConsumer?}
-M.blocking_spawn = function(cmds, opts)
-  ---@diagnostic disable-next-line: inject-field
-  opts.blocking = true
-  ---@diagnostic disable-next-line: param-type-mismatch
-  return M.spawn(cmds, opts)
-end
-
---- @param cmds string[]
---- @param opts {on_stdout:fzfx.SpawnLineConsumer, on_stderr:fzfx.SpawnLineConsumer?}
-M.nonblocking_spawn = function(cmds, opts)
-  ---@diagnostic disable-next-line: inject-field
-  opts.blocking = false
-  ---@diagnostic disable-next-line: param-type-mismatch
-  return M.spawn(cmds, opts)
-end
+M.Spawn = Spawn
 
 return M
---
+++ test/lib/spawn_spec.lua
@@ -0,0 +1,230 @@
+---@diagnostic disable: undefined-field, unused-local
+local cwd = vim.fn.getcwd()
+
+describe("lib.spawn", function()
+  local assert_eq = assert.is_equal
+  local assert_true = assert.is_true
+  local assert_false = assert.is_false
+
+  before_each(function()
+    vim.api.nvim_command("cd " .. cwd)
+  end)
+
+  local function dummy() end
+
+  local strs = require("fzfx.lib.strings")
+  local fs = require("fzfx.lib.files")
+  local spawn = require("fzfx.lib.spawn")
+
+  local dummy = function() end
+
+  describe("[blocking Spawn]", function()
+    it("open", function()
+      local sp = spawn.Spawn:make(
+        { "cat", "README.md" },
+        { on_stdout = dummy, blocking = true }
+      )
+      assert_eq(type(sp), "table")
+      assert_eq(type(sp.cmds), "table")
+      assert_eq(#sp.cmds, 2)
+      assert_eq(sp.cmds[1], "cat")
+      assert_eq(sp.cmds[2], "README.md")
+      assert_eq(type(sp.out_pipe), "userdata")
+      assert_eq(type(sp.err_pipe), "userdata")
+    end)
+    it("consume line", function()
+      local content = fs.readfile("README.md") --[[@as string]]
+      local lines = fs.readlines("README.md") --[[@as table]]
+
+      local i = 1
+      local function process_line(line)
+        -- print(string.format("[%d]%s", i, line))
+        assert_eq(type(line), "string")
+        assert_eq(line, lines[i])
+        i = i + 1
+      end
+      local sp = spawn.Spawn:make(
+        { "cat", "README.md" },
+        { on_stdout = process_line, blocking = true }
+      )
+      local pos = sp:_consume_line(content, process_line)
+      if pos <= #content then
+        local line = content:sub(pos, #content)
+        process_line(line)
+      end
+    end)
+    it("stdout on newline", function()
+      local content = fs.readfile("README.md") --[[@as string]]
+      local lines = fs.readlines("README.md") --[[@as table]]
+
+      local i = 1
+      local function process_line(line)
+        -- print(string.format("[%d]%s\n", i, line))
+        assert_eq(type(line), "string")
+        assert_eq(line, lines[i])
+        i = i + 1
+      end
+      local sp = spawn.Spawn:make(
+        { "cat", "README.md" },
+        { on_stdout = process_line, blocking = true }
+      )
+      local content_splits = strs.split(content, "\n", { trimempty = false })
+      for j, splits in ipairs(content_splits) do
+        sp:_on_stdout(nil, splits)
+        if j < #content_splits then
+          sp:_on_stdout(nil, "\n")
+        end
+      end
+      sp:_on_stdout(nil, nil)
+      assert_true(sp.out_pipe:is_closing())
+    end)
+    it("stdout on whitespace", function()
+      local content = fs.readfile("README.md") --[[@as string]]
+      local lines = fs.readlines("README.md") --[[@as table]]
+
+      local i = 1
+      local function process_line(line)
+        -- print(string.format("[%d]%s\n", i, line))
+        assert_eq(type(line), "string")
+        assert_eq(line, lines[i])
+        i = i + 1
+      end
+      local sp = spawn.Spawn:make(
+        { "cat", "README.md" },
+        { on_stdout = process_line, blocking = true }
+      )
+      local content_splits = strs.split(content, " ", { trimempty = false })
+      for j, splits in ipairs(content_splits) do
+        sp:_on_stdout(nil, splits)
+        if j < #content_splits then
+          sp:_on_stdout(nil, " ")
+        end
+      end
+      sp:_on_stdout(nil, nil)
+      assert_true(sp.out_pipe:is_closing())
+    end)
+    local delimiter_i = 0
+    while delimiter_i <= 25 do
+      -- lower case: a
+      local lower_char = string.char(97 + delimiter_i)
+      it(string.format("stdout on %s", lower_char), function()
+        local content = fs.readfile("README.md") --[[@as string]]
+        local lines = fs.readlines("README.md") --[[@as table]]
+
+        local i = 1
+        local function process_line(line)
+          -- print(string.format("[%d]%s\n", i, line))
+          assert_eq(type(line), "string")
+          assert_eq(line, lines[i])
+          i = i + 1
+        end
+        local sp = spawn.Spawn:make(
+          { "cat", "README.md" },
+          { on_stdout = process_line, on_stderr = dummy, blocking = true }
+        )
+        local content_splits =
+          strs.split(content, lower_char, { trimempty = false })
+        for j, splits in ipairs(content_splits) do
+          sp:_on_stdout(nil, splits)
+          if j < #content_splits then
+            sp:_on_stdout(nil, lower_char)
+          end
+        end
+        sp:_on_stdout(nil, nil)
+        assert_true(sp.out_pipe:is_closing())
+      end)
+      -- upper case: A
+      local upper_char = string.char(65 + delimiter_i)
+      it(string.format("stdout on %s", upper_char), function()
+        local content = fs.readfile("README.md") --[[@as string]]
+        local lines = fs.readlines("README.md") --[[@as table]]
+
+        local i = 1
+        local function process_line(line)
+          -- print(string.format("[%d]%s\n", i, line))
+          assert_eq(type(line), "string")
+          assert_eq(line, lines[i])
+          i = i + 1
+        end
+        local sp = spawn.Spawn:make(
+          { "cat", "README.md" },
+          { on_stdout = process_line, on_stderr = dummy, blocking = true }
+        )
+        local content_splits =
+          strs.split(content, upper_char, { trimempty = false })
+        for j, splits in ipairs(content_splits) do
+          sp:_on_stdout(nil, splits)
+          if j < #content_splits then
+            sp:_on_stdout(nil, upper_char)
+          end
+        end
+        sp:_on_stdout(nil, nil)
+        assert_true(sp.out_pipe:is_closing())
+      end)
+      delimiter_i = delimiter_i + math.random(1, 5)
+    end
+    it("stderr", function()
+      local sp = spawn.Spawn:make(
+        { "cat", "README.md" },
+        { on_stdout = dummy, on_stderr = dummy, blocking = true }
+      )
+      sp:_on_stderr(nil, nil)
+      assert_true(sp.err_pipe:is_closing())
+    end)
+    it("stderr2", function()
+      local i = 1
+      local function process_line(line)
+        -- print(string.format("process[%d]:%s\n", i, line))
+      end
+      local sp = spawn.Spawn:make(
+        { "cat", "non_exists.txt" },
+        { on_stdout = process_line, on_stderr = process_line, blocking = true }
+      )
+      sp:run()
+    end)
+    it("iterate on README.md", function()
+      local lines = fs.readlines("README.md") --[[@as table]]
+
+      local i = 1
+      local function process_line(line)
+        -- print(string.format("[%d]%s\n", i, line))
+        assert_eq(type(line), "string")
+        assert_eq(lines[i], line)
+        i = i + 1
+      end
+
+      local sp = spawn.Spawn:make(
+        { "cat", "README.md" },
+        { on_stdout = process_line, on_stderr = dummy, blocking = true }
+      )
+      sp:run()
+    end)
+    it("iterate on lua/fzfx/config.lua", function()
+      local lines = fs.readlines("lua/fzfx/config.lua") --[[@as table]]
+
+      local i = 1
+      local function process_line(line)
+        -- print(string.format("[%d]%s\n", i, line))
+        assert_eq(type(line), "string")
+        assert_eq(lines[i], line)
+        i = i + 1
+      end
+
+      local sp = spawn.Spawn:make(
+        { "cat", "lua/fzfx/config.lua" },
+        { on_stdout = process_line, on_stderr = dummy, blocking = true }
+      )
+      sp:run()
+    end)
+    it("close handle", function()
+      local sp = spawn.Spawn:make(
+        { "cat", "lua/fzfx/config.lua" },
+        { on_stdout = dummy, on_stderr = dummy, blocking = true }
+      )
+      sp:run()
+      assert_true(sp.process_handle ~= nil)
+      sp:_close_handle(sp.process_handle)
+      assert_true(sp.process_handle:is_closing())
+    end)
+  end)
+end)
