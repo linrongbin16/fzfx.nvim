bd50b24f47757bc5e7f28c00294e068b07b06afa
linrongbin16 <linrongbin16@outlook.com>
2023-11-28 12:35:49 +0800

    chore

--- test/lib/filesystem_spec.lua
+++ test/lib/filesystem_spec.lua
@@ -41,4 +41,37 @@ describe("lib.filesystem", function()
       assert_eq(strings.rtrim(buffer --[[@as string]]), content)
     end)
   end)
+  describe("[writefile/writelines]", function()
+    it("writefile and writelines", function()
+      local content = fs.readfile("README.md") --[[@as string]]
+      local lines = fs.readlines("README.md") --[[@as table]]
+
+      local t1 = "test1-README.md"
+      local t2 = "test2-README.md"
+      fs.writefile(t1, content)
+      fs.writelines(t2, lines)
+
+      content = fs.readfile("test1-README.md") --[[@as string]]
+      lines = fs.readlines("test2-README.md") --[[@as table]]
+
+      local buffer = nil
+      for _, line in
+        ipairs(lines --[[@as table]])
+      do
+        assert_eq(type(line), "string")
+        assert_true(string.len(line) >= 0)
+        buffer = buffer and (buffer .. line .. "\n") or (line .. "\n")
+      end
+      assert_eq(strings.rtrim(buffer --[[@as string]]), content)
+      local j1 = vim.fn.jobstart(
+        { "rm", t1 },
+        { on_stdout = function() end, on_stderr = function() end }
+      )
+      local j2 = vim.fn.jobstart(
+        { "rm", t2 },
+        { on_stdout = function() end, on_stderr = function() end }
+      )
+      vim.fn.jobwait({ j1, j2 })
+    end)
+  end)
 end)
