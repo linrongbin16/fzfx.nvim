508526a58c4fbae62122eb77d272a9f4b69cf60b
linrongbin16 <linrongbin16@outlook.com>
2023-11-28 13:43:11 +0800

    chore

--- test/lib/filesystems_spec.lua
+++ test/lib/filesystems_spec.lua
@@ -10,13 +10,13 @@ describe("lib.filesystems", function()
     vim.api.nvim_command("cd " .. cwd)
   end)
 
-  local libstr = require("fzfx.lib.strings")
-  local libfs = require("fzfx.lib.filesystems")
+  local strs = require("fzfx.lib.strings")
+  local fs = require("fzfx.lib.filesystems")
 
   describe("[readfile/readlines]", function()
     it("readfile and FileLineReader", function()
-      local content = libfs.readfile("README.md")
-      local reader = libfs.FileLineReader:open("README.md") --[[@as FileLineReader]]
+      local content = fs.readfile("README.md")
+      local reader = fs.FileLineReader:open("README.md") --[[@as FileLineReader]]
       local buffer = nil
       assert_eq(type(reader), "table")
       while reader:has_next() do
@@ -26,11 +26,11 @@ describe("lib.filesystems", function()
         buffer = buffer and (buffer .. line .. "\n") or (line .. "\n")
       end
       reader:close()
-      assert_eq(libstr.rtrim(buffer --[[@as string]]), content)
+      assert_eq(strs.rtrim(buffer --[[@as string]]), content)
     end)
     it("readfile and readlines", function()
-      local content = libfs.readfile("README.md")
-      local lines = libfs.readlines("README.md")
+      local content = fs.readfile("README.md")
+      local lines = fs.readlines("README.md")
       local buffer = nil
       for _, line in
         ipairs(lines --[[@as table]])
@@ -39,21 +39,21 @@ describe("lib.filesystems", function()
         assert_true(string.len(line) >= 0)
         buffer = buffer and (buffer .. line .. "\n") or (line .. "\n")
       end
-      assert_eq(libstr.rtrim(buffer --[[@as string]]), content)
+      assert_eq(strs.rtrim(buffer --[[@as string]]), content)
     end)
   end)
   describe("[writefile/writelines]", function()
     it("writefile and writelines", function()
-      local content = libfs.readfile("README.md") --[[@as string]]
-      local lines = libfs.readlines("README.md") --[[@as table]]
+      local content = fs.readfile("README.md") --[[@as string]]
+      local lines = fs.readlines("README.md") --[[@as table]]
 
       local t1 = "writefile-test1-README.md"
       local t2 = "writefile-test2-README.md"
-      libfs.writefile(t1, content)
-      libfs.writelines(t2, lines)
+      fs.writefile(t1, content)
+      fs.writelines(t2, lines)
 
-      content = libfs.readfile(t1) --[[@as string]]
-      lines = libfs.readlines(t2) --[[@as table]]
+      content = fs.readfile(t1) --[[@as string]]
+      lines = fs.readlines(t2) --[[@as table]]
 
       local buffer = nil
       for _, line in
@@ -63,7 +63,7 @@ describe("lib.filesystems", function()
         assert_true(string.len(line) >= 0)
         buffer = buffer and (buffer .. line .. "\n") or (line .. "\n")
       end
-      assert_eq(libstr.rtrim(buffer --[[@as string]]), content)
+      assert_eq(strs.rtrim(buffer --[[@as string]]), content)
       local j1 = vim.fn.jobstart(
         { "rm", t1 },
         { on_stdout = function() end, on_stderr = function() end }
@@ -79,7 +79,7 @@ describe("lib.filesystems", function()
     it("write", function()
       local t = "asyncwritefile-test.txt"
       local content = "hello world, goodbye world!"
-      libfs.asyncwritefile(t, content, function(err, bytes)
+      fs.asyncwritefile(t, content, function(err, bytes)
         assert_true(err == nil)
         assert_eq(bytes, #content)
       end)
@@ -88,7 +88,7 @@ describe("lib.filesystems", function()
   describe("[asyncreadfile]", function()
     it("read", function()
       local t = "README.md"
-      libfs.asyncreadfile(t, function(content)
+      fs.asyncreadfile(t, function(content)
         assert_true(string.len(content) > 0)
       end)
     end)
