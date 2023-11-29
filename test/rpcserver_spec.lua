163532c248d13d803fd7224b5a51cd088ad0d11f
linrongbin16 <linrongbin16@outlook.com>
2023-11-29 13:29:15 +0800

    chore

--- test/rpcserver_spec.lua
+++ test/rpcserver_spec.lua
@@ -1,6 +1,7 @@
+---@diagnostic disable: undefined-field, unused-local
 local cwd = vim.fn.getcwd()
 
-describe("server", function()
+describe("rpcserver", function()
   local assert_eq = assert.is_equal
   local assert_true = assert.is_true
   local assert_false = assert.is_false
@@ -10,11 +11,11 @@ describe("server", function()
   end)
 
   local nums = require("fzfx.lib.numbers")
-  local server = require("fzfx.server")
-  server.setup()
+  local rpcserver = require("fzfx.rpcserver")
+  rpcserver.setup()
   describe("[RpcServer]", function()
     it("create server", function()
-      local s = server.get_rpc_server()
+      local s = rpcserver.get_instance()
       assert_eq(type(s), "table")
       local sockaddr = vim.env._FZFX_NVIM_RPC_SERVER_ADDRESS
       print(string.format("rpc server socket:%s\n", vim.inspect(sockaddr)))
@@ -22,11 +23,11 @@ describe("server", function()
       assert_true(string.len(sockaddr) > 0)
     end)
     it("register/unregister", function()
-      local s = server.get_rpc_server()
+      local s = rpcserver.get_instance()
       local f = function(x) end
       local rid = s:register(f)
-      assert_eq(type(rid), "number")
-      assert_true(rid == nums.inc_id() - 1)
+      assert_eq(type(rid), "string")
+      assert_true(tonumber(rid) == nums.inc_id() - 1)
       local actual1 = s:get(rid)
       assert_eq(type(actual1), "function")
       assert_eq(vim.inspect(f), vim.inspect(actual1))
