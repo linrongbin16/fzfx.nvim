35789ad834e1c1f65e497800a5f691227add6d63
linrongbin16 <linrongbin16@outlook.com>
2023-11-30 10:04:01 +0800

    chore

---
+++ lua/fzfx/helper/prompts.lua
@@ -0,0 +1,34 @@
+local strs = require("fzfx.lib.strings")
+local nvims = require("fzfx.lib.nvims")
+
+local log = require("fzfx.log")
+local LogLevels = require("fzfx.log").LogLevels
+
+local M = {}
+
+local CANCELLED_MESSAGE = "cancelled."
+
+--- @param bufnr integer
+--- @param callback fun():any
+M.confirm_discard_buffer_modified = function(bufnr, callback)
+  if not vim.o.hidden and nvims.get_buf_option(bufnr, "modified") then
+    local ok, input = pcall(vim.fn.input, {
+      prompt = "[fzfx] buffer has been modified, continue? (y/n) ",
+      cancelreturn = "n",
+    })
+    if
+      ok
+      and type(input) == "string"
+      and string.len(input) > 0
+      and strs.startswith(input:lower(), "y")
+    then
+      callback()
+    else
+      log.echo(LogLevels.INFO, CANCELLED_MESSAGE)
+    end
+  else
+    callback()
+  end
+end
+
+return M
