ad9365070d69534ede1929c46f6ffc7a120d7baa
linrongbin16 <linrongbin16@outlook.com>
2023-12-05 16:19:26 +0800

    perf(git_live_grep): migrate 'git_live_grep' to 'cfg.git_live_grep' (#451)
    docs(references): update wording

---
+++ lua/fzfx/cfg/git_live_grep.lua
@@ -0,0 +1,138 @@
+local consts = require("fzfx.lib.constants")
+local strs = require("fzfx.lib.strings")
+local nvims = require("fzfx.lib.nvims")
+local cmds = require("fzfx.lib.commands")
+local colors = require("fzfx.lib.colors")
+local paths = require("fzfx.lib.paths")
+local fs = require("fzfx.lib.filesystems")
+local tbls = require("fzfx.lib.tables")
+local log = require("fzfx.lib.log")
+local LogLevels = require("fzfx.lib.log").LogLevels
+
+local parsers_helper = require("fzfx.helper.parsers")
+local queries_helper = require("fzfx.helper.queries")
+local actions_helper = require("fzfx.helper.actions")
+local labels_helper = require("fzfx.helper.previewer_labels")
+local providers_helper = require("fzfx.helper.providers")
+local previewers_helper = require("fzfx.helper.previewers")
+
+local ProviderTypeEnum = require("fzfx.schema").ProviderTypeEnum
+local PreviewerTypeEnum = require("fzfx.schema").PreviewerTypeEnum
+local CommandFeedEnum = require("fzfx.schema").CommandFeedEnum
+
+local M = {}
+
+M.commands = {
+  -- normal
+  {
+    name = "FzfxGLiveGrep",
+    feed = CommandFeedEnum.ARGS,
+    opts = {
+      bang = true,
+      nargs = "*",
+      desc = "Git live grep",
+    },
+  },
+  -- visual
+  {
+    name = "FzfxGLiveGrepV",
+    feed = CommandFeedEnum.VISUAL,
+    opts = {
+      bang = true,
+      range = true,
+      desc = "Git live grep by visual select",
+    },
+  },
+  -- cword
+  {
+    name = "FzfxGLiveGrepW",
+    feed = CommandFeedEnum.CWORD,
+    opts = {
+      bang = true,
+      desc = "Git live grep by cursor word",
+    },
+  },
+  -- put
+  {
+    name = "FzfxGLiveGrepP",
+    feed = CommandFeedEnum.PUT,
+    opts = {
+      bang = true,
+      desc = "Git live grep by yank text",
+    },
+  },
+  -- resume
+  {
+    name = "FzfxGLiveGrepR",
+    feed = CommandFeedEnum.RESUME,
+    opts = {
+      bang = true,
+      desc = "Git live grep by resume last",
+    },
+  },
+}
+
+--- @param query string?
+--- @param context fzfx.PipelineContext
+--- @return string[]|nil
+M._git_live_grep_provider = function(query, context)
+  local git_root_cmd = cmds.GitRootCommand:run()
+  if git_root_cmd:failed() then
+    log.echo(LogLevels.INFO, "not in git repo.")
+    return nil
+  end
+
+  local parsed = queries_helper.parse_flagged(query or "")
+  local payload = parsed.payload
+  local option = parsed.option
+
+  local args = { "git", "grep", "--color=always", "-n" }
+  if type(option) == "string" and string.len(option) > 0 then
+    local option_splits = strs.split(option, " ")
+    for _, o in ipairs(option_splits) do
+      if type(o) == "string" and string.len(o) > 0 then
+        table.insert(args, o)
+      end
+    end
+  end
+  table.insert(args, payload)
+  return args
+end
+
+M.providers = {
+  key = "default",
+  provider = M._git_live_grep_provider,
+  provider_type = ProviderTypeEnum.COMMAND_LIST,
+  line_opts = {
+    prepend_icon_by_ft = true,
+    prepend_icon_path_delimiter = ":",
+    prepend_icon_path_position = 1,
+  },
+}
+
+M.previewers = {
+  previewer = previewers_helper.preview_files_grep,
+  previewer_type = PreviewerTypeEnum.COMMAND_LIST,
+  previewer_label = labels_helper.label_grep,
+}
+
+M.actions = {
+  ["esc"] = actions_helper.nop,
+  ["enter"] = actions_helper.edit_grep,
+  ["double-click"] = actions_helper.edit_grep,
+  ["ctrl-q"] = actions_helper.setqflist_grep,
+}
+
+M.fzf_opts = {
+  consts.FZF_OPTS.MULTI,
+  consts.FZF_OPTS.DISABLED,
+  consts.FZF_OPTS.DELIMITER,
+  { "--prompt", "Git Live Grep > " },
+  { "--preview-window", "+{2}-/2" },
+}
+
+M.other_opts = {
+  reload_on_change = true,
+}
+
+return M
---
+++ test/cfg/git_live_grep_spec.lua
@@ -0,0 +1,49 @@
+---@diagnostic disable: undefined-field, unused-local, missing-fields, need-check-nil, param-type-mismatch, assign-type-mismatch
+local cwd = vim.fn.getcwd()
+
+describe("cfg.git_live_grep", function()
+  local assert_eq = assert.is_equal
+  local assert_true = assert.is_true
+  local assert_false = assert.is_false
+
+  before_each(function()
+    vim.api.nvim_command("cd " .. cwd)
+    vim.o.swapfile = false
+    vim.cmd([[edit README.md]])
+  end)
+
+  local github_actions = os.getenv("GITHUB_ACTIONS") == "true"
+
+  local tbls = require("fzfx.lib.tables")
+  local consts = require("fzfx.lib.constants")
+  local strs = require("fzfx.lib.strings")
+  local paths = require("fzfx.lib.paths")
+  local colors = require("fzfx.lib.colors")
+
+  local contexts = require("fzfx.helper.contexts")
+  local providers = require("fzfx.helper.providers")
+  local fzf_helpers = require("fzfx.detail.fzf_helpers")
+
+  local git_live_grep_cfg = require("fzfx.cfg.git_live_grep")
+
+  describe("git_live_grep", function()
+    it("_git_live_grep_provider", function()
+      local actual = git_live_grep_cfg._git_live_grep_provider("", {})
+      print(string.format("git live grep:%s\n", vim.inspect(actual)))
+      if actual ~= nil then
+        assert_eq(type(actual), "table")
+        assert_eq(actual[1], "git")
+        assert_eq(actual[2], "grep")
+      end
+    end)
+    it("_git_live_grep_provider with -- flag", function()
+      local actual = git_live_grep_cfg._git_live_grep_provider("fzfx -- -v", {})
+      print(string.format("git live grep:%s\n", vim.inspect(actual)))
+      if actual ~= nil then
+        assert_eq(type(actual), "table")
+        assert_eq(actual[1], "git")
+        assert_eq(actual[2], "grep")
+      end
+    end)
+  end)
+end)
