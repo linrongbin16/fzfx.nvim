fd3640de4d0c7e268e2abc5c3c9f22bdd7c490a0
linrongbin16 <linrongbin16@outlook.com>
2023-12-05 17:15:19 +0800

    perf(git_status): migrate 'git_status' to 'cfg.git_status' (#453)

---
+++ lua/fzfx/cfg/git_status.lua
@@ -0,0 +1,217 @@
+local consts = require("fzfx.lib.constants")
+local strs = require("fzfx.lib.strings")
+local nvims = require("fzfx.lib.nvims")
+local cmds = require("fzfx.lib.commands")
+local colors = require("fzfx.lib.colors")
+local paths = require("fzfx.lib.paths")
+local fs = require("fzfx.lib.filesystems")
+local tbls = require("fzfx.lib.tables")
+local log = require("fzfx.lib.log")
+local LogLevels = require("fzfx.lib.log").LogLevels
+
+local parsers_helper = require("fzfx.helper.parsers")
+local queries_helper = require("fzfx.helper.queries")
+local actions_helper = require("fzfx.helper.actions")
+local labels_helper = require("fzfx.helper.previewer_labels")
+local providers_helper = require("fzfx.helper.providers")
+local previewers_helper = require("fzfx.helper.previewers")
+
+local ProviderTypeEnum = require("fzfx.schema").ProviderTypeEnum
+local PreviewerTypeEnum = require("fzfx.schema").PreviewerTypeEnum
+local CommandFeedEnum = require("fzfx.schema").CommandFeedEnum
+
+local M = {}
+
+M.commands = {
+  -- normal
+  {
+    name = "FzfxGStatus",
+    feed = CommandFeedEnum.ARGS,
+    opts = {
+      bang = true,
+      nargs = "?",
+      complete = "dir",
+      desc = "Find changed git files (status)",
+    },
+    default_provider = "workspace",
+  },
+  {
+    name = "FzfxGStatusC",
+    feed = CommandFeedEnum.ARGS,
+    opts = {
+      bang = true,
+      nargs = "?",
+      complete = "dir",
+      desc = "Find changed git files (status) in current directory",
+    },
+    default_provider = "current_folder",
+  },
+  -- visual
+  {
+    name = "FzfxGStatusV",
+    feed = CommandFeedEnum.VISUAL,
+    opts = {
+      bang = true,
+      range = true,
+      desc = "Find changed git files (status) by visual select",
+    },
+    default_provider = "workspace",
+  },
+  {
+    name = "FzfxGStatusCV",
+    feed = CommandFeedEnum.VISUAL,
+    opts = {
+      bang = true,
+      range = true,
+      desc = "Find changed git files (status) in current directory by visual select",
+    },
+    default_provider = "current_folder",
+  },
+  -- cword
+  {
+    name = "FzfxGStatusW",
+    feed = CommandFeedEnum.CWORD,
+    opts = {
+      bang = true,
+      desc = "Find changed git files (status) by cursor word",
+    },
+    default_provider = "workspace",
+  },
+  {
+    name = "FzfxGStatusCW",
+    feed = CommandFeedEnum.CWORD,
+    opts = {
+      bang = true,
+      desc = "Find changed git files (status) in current directory by cursor word",
+    },
+    default_provider = "current_folder",
+  },
+  -- put
+  {
+    name = "FzfxGStatusP",
+    feed = CommandFeedEnum.PUT,
+    opts = {
+      bang = true,
+      desc = "Find changed git files (status) by yank text",
+    },
+    default_provider = "workspace",
+  },
+  {
+    name = "FzfxGStatusCP",
+    feed = CommandFeedEnum.PUT,
+    opts = {
+      bang = true,
+      desc = "Find changed git files (status) in current directory by yank text",
+    },
+    default_provider = "current_folder",
+  },
+  -- resume
+  {
+    name = "FzfxGStatusR",
+    feed = CommandFeedEnum.RESUME,
+    opts = {
+      bang = true,
+      desc = "Find changed git files (status) by resume last",
+    },
+    default_provider = "workspace",
+  },
+  {
+    name = "FzfxGStatusCR",
+    feed = CommandFeedEnum.RESUME,
+    opts = {
+      bang = true,
+      desc = "Find changed git files (status) in current directory by resume last",
+    },
+    default_provider = "current_folder",
+  },
+}
+
+--- @param opts {current_folder:boolean?}?
+--- @return boolean
+M._is_current_folder_mode = function(opts)
+  ---@diagnostic disable-next-line: need-check-nil
+  return tbls.tbl_not_empty(opts) and opts.current_folder --[[@as boolean]]
+end
+
+--- @param opts {current_folder:boolean?}?
+--- @return fun():string[]|nil
+M._make_git_status_provider = function(opts)
+  local function impl()
+    local git_root_cmd = cmds.GitRootCommand:run()
+    if git_root_cmd:failed() then
+      log.echo(LogLevels.INFO, "not in git repo.")
+      return nil
+    end
+    return M._is_current_folder_mode(opts)
+        and {
+          "git",
+          "-c",
+          "color.status=always",
+          "status",
+          "--short",
+          ".",
+        }
+      or { "git", "-c", "color.status=always", "status", "--short" }
+  end
+  return impl
+end
+
+local current_folder_provider =
+  M._make_git_status_provider({ current_folder = true })
+local workspace_provider = M._make_git_status_provider()
+
+M.providers = {
+  current_folder = {
+    key = "ctrl-u",
+    provider = current_folder_provider,
+    provider_type = ProviderTypeEnum.COMMAND_LIST,
+  },
+  workspace = {
+    key = "ctrl-w",
+    provider = workspace_provider,
+    provider_type = ProviderTypeEnum.COMMAND_LIST,
+  },
+}
+
+--- @param line string
+--- @return string?
+M._git_status_previewer = function(line)
+  local parsed = parsers_helper.parse_git_status(line)
+  if consts.HAS_DELTA then
+    local win_width = previewers_helper.get_preview_window_width()
+    return string.format(
+      [[git diff %s | delta -n --tabs 4 --width %d]],
+      nvims.shellescape(parsed.filename),
+      win_width
+    )
+  else
+    return string.format(
+      [[git diff --color=always %s]],
+      nvims.shellescape(parsed.filename)
+    )
+  end
+end
+
+M.previewers = {
+  current_folder = {
+    previewer = M._git_status_previewer,
+  },
+  workspace = {
+    previewer = M._git_status_previewer,
+  },
+}
+
+M.actions = {
+  ["esc"] = actions_helper.nop,
+  ["enter"] = actions_helper.edit_git_status,
+  ["double-click"] = actions_helper.edit_git_status,
+  ["ctrl-q"] = actions_helper.setqflist_git_status,
+}
+
+M.fzf_opts = {
+  consts.FZF_OPTS.MULTI,
+  { "--preview-window", "wrap" },
+  { "--prompt", "Git Status > " },
+}
+
+return M
---
+++ test/cfg/git_status_spec.lua
@@ -0,0 +1,76 @@
+---@diagnostic disable: undefined-field, unused-local, missing-fields, need-check-nil, param-type-mismatch, assign-type-mismatch
+local cwd = vim.fn.getcwd()
+
+describe("cfg.git_live_grep", function()
+  local assert_eq = assert.is_equal
+  local assert_true = assert.is_true
+  local assert_false = assert.is_false
+
+  before_each(function()
+    vim.api.nvim_command("cd " .. cwd)
+    vim.o.swapfile = false
+    vim.cmd([[edit README.md]])
+  end)
+
+  local github_actions = os.getenv("GITHUB_ACTIONS") == "true"
+
+  local tbls = require("fzfx.lib.tables")
+  local consts = require("fzfx.lib.constants")
+  local strs = require("fzfx.lib.strings")
+  local paths = require("fzfx.lib.paths")
+  local colors = require("fzfx.lib.colors")
+
+  local contexts = require("fzfx.helper.contexts")
+  local providers = require("fzfx.helper.providers")
+  local fzf_helpers = require("fzfx.detail.fzf_helpers")
+
+  local git_status_cfg = require("fzfx.cfg.git_status")
+
+  describe("[git_status]", function()
+    it("_git_status_previewer", function()
+      local lines = {
+        " M fzfx/config.lua",
+        " D fzfx/constants.lua",
+        " M fzfx/line_helpers.lua",
+        " M ../test/line_helpers_spec.lua",
+        "?? ../hello",
+      }
+      for _, line in ipairs(lines) do
+        local actual = git_status_cfg._git_status_previewer(line)
+        assert_eq(type(actual), "string")
+        assert_true(strs.find(actual, "git diff") > 0)
+        if vim.fn.executable("delta") > 0 then
+          assert_true(strs.find(actual, "delta") > 0)
+        else
+          assert_true(strs.find(actual, "delta") == nil)
+        end
+      end
+    end)
+    it("_make_git_status_provider", function()
+      local actual1 = git_status_cfg._make_git_status_provider({})()
+      local actual2 =
+        git_status_cfg._make_git_status_provider({ current_folder = true })()
+      -- print(
+      --     string.format("git status provider1:%s\n", vim.inspect(actual1))
+      -- )
+      -- print(
+      --     string.format("git status provider2:%s\n", vim.inspect(actual2))
+      -- )
+      assert_true(actual1 == nil or vim.deep_equal(actual1, {
+        "git",
+        "-c",
+        "color.status=always",
+        "status",
+        "--short",
+      }))
+      assert_true(actual2 == nil or vim.deep_equal(actual2, {
+        "git",
+        "-c",
+        "color.status=always",
+        "status",
+        "--short",
+        ".",
+      }))
+    end)
+  end)
+end)
