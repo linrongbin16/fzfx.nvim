26fc0c8596e5e2a24f4d1983856f60b1dc7924d0
linrongbin16 <linrongbin16@outlook.com>
2023-12-07 11:47:32 +0800

    chore

--- lua/fzfx/cfg/files.lua
+++ lua/fzfx/cfg/files.lua
@@ -135,12 +135,14 @@ M.providers = {
   restricted_mode = {
     key = "ctrl-r",
     provider = restricted_provider,
-    line_opts = { prepend_icon_by_ft = true },
+    -- line_opts = { prepend_icon_by_ft = true },
+    provider_decorator = { "fzfx.helper.provider_decorators.prepend_icon_find" },
   },
   unrestricted_mode = {
     key = "ctrl-u",
     provider = unrestricted_provider,
-    line_opts = { prepend_icon_by_ft = true },
+    -- line_opts = { prepend_icon_by_ft = true },
+    provider_decorator = { "fzfx.helper.provider_decorators.prepend_icon_find" },
   },
 }
 
