1d930871fd404f2b77cc7c06871bb3eb46ec76c1
linrongbin16 <linrongbin16@outlook.com>
2023-12-19 14:03:17 +0800

    chore

--- test/cfg/buffers_spec.lua
+++ test/cfg/buffers_spec.lua
@@ -14,6 +14,7 @@ describe("cfg.buffers", function()
 
   local github_actions = os.getenv("GITHUB_ACTIONS") == "true"
 
+  require("fzfx").setup()
   local tbls = require("fzfx.lib.tables")
   local consts = require("fzfx.lib.constants")
   local strs = require("fzfx.lib.strings")
