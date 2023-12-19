0d589a9169ba3b5a6b4f5a6bc735195e3ad5e56f
linrongbin16 <linrongbin16@outlook.com>
2023-12-19 10:43:49 +0800

    chore

--- lua/fzfx/lib/log.lua
+++ lua/fzfx/lib/log.lua
@@ -1,165 +1,153 @@
-local strs = require("fzfx.lib.strings")
-local paths = require("fzfx.lib.paths")
-
 local M = {}
 
--- see: `lua print(vim.inspect(vim.log.levels))`
-M.LogLevels = {
-  TRACE = 0,
-  DEBUG = 1,
-  INFO = 2,
-  WARN = 3,
-  ERROR = 4,
-  OFF = 5,
-}
+M.LogLevels = require("fzfx.commons.logging").LogLevels
 
-M.LogLevelNames = {
-  [0] = "TRACE",
-  [1] = "DEBUG",
-  [2] = "INFO",
-  [3] = "WARN",
-  [4] = "ERROR",
-  [5] = "OFF",
-}
+M.LogLevelNames = require("fzfx.commons.logging").LogLevelNames
 
 local LogHighlights = {
+  [0] = "Comment",
   [1] = "Comment",
   [2] = "None",
   [3] = "WarningMsg",
   [4] = "ErrorMsg",
+  [5] = "ErrorMsg",
 }
 
 --- @param level integer
 --- @param fmt string
 --- @param ... any
 M.echo = function(level, fmt, ...)
+  local numbers = require("fzfx.commons.numbers")
+  level = numbers.bound(level, M.LogLevels.TRACE, M.LogLevels.OFF)
+
   local msg = string.format(fmt, ...)
-  local msg_lines = strs.split(msg, "\n")
-  local msg_chunks = {}
+  local msg_lines = vim.split(msg, "\n", { plain = true })
+
   local prefix = ""
   if level == M.LogLevels.ERROR then
     prefix = "error! "
   elseif level == M.LogLevels.WARN then
     prefix = "warning! "
   end
+
   for _, line in ipairs(msg_lines) do
-    table.insert(msg_chunks, {
+    local chunks = {}
+    table.insert(chunks, {
       string.format("[fzfx] %s%s", prefix, line),
       LogHighlights[level],
     })
+    vim.schedule(function()
+      vim.api.nvim_echo(chunks, false, {})
+    end)
   end
-  vim.schedule(function()
-    vim.api.nvim_echo(msg_chunks, false, {})
-  end)
 end
 
 --- @type fzfx.Options
 local Defaults = {
   level = M.LogLevels.INFO,
-  console_log = true,
   name = "[fzfx]",
+  console_log = true,
   file_log = false,
   file_name = "fzfx.log",
   file_dir = vim.fn.stdpath("data"),
   file_path = nil,
 }
 
---- @type fzfx.Options
-local Configs = {
-  level = M.LogLevels.DEBUG,
-  console_log = true,
-  name = "[fzfx-safe-mode]",
-  file_log = false,
-  file_name = "fzfx_safe_mode.log",
-  file_dir = vim.fn.stdpath("data"),
-  file_path = string.format(
-    "%s%s%s",
-    vim.fn.stdpath("data"),
-    paths.SEPARATOR,
-    "fzfx_safe_mode.log"
-  ),
-}
-
---- @param option fzfx.Options
-M.setup = function(option)
-  Configs = vim.tbl_deep_extend("force", vim.deepcopy(Defaults), option or {})
-  if type(Configs.level) == "string" then
-    Configs.level = M.LogLevels[Configs.level]
-  end
-  assert(type(Configs.name) == "string")
-  assert(string.len(Configs.name) > 0)
-  assert(type(Configs.level) == "number")
-  if Configs.file_log then
-    Configs.file_path = string.format(
-      "%s%s%s",
-      Configs.file_dir,
-      paths.SEPARATOR,
-      Configs.file_name
-    )
-    assert(type(Configs.file_path) == "string")
-    assert(string.len(Configs.file_path) > 0)
-  end
-end
-
---- @param level integer
---- @param msg string
-local function _log(level, msg)
-  if level < Configs.level then
-    return
-  end
-
-  local msg_lines = strs.split(msg, "\n")
-  if Configs.console_log and level >= M.LogLevels.INFO then
-    M.echo(level, msg)
-  end
-  if Configs.file_log then
-    local fp = io.open(Configs.file_path, "a")
-    if fp then
-      for _, line in ipairs(msg_lines) do
-        local secs, ms = vim.loop.gettimeofday()
-        fp:write(
-          string.format(
-            "%s.%03d [%s]: %s\n",
-            os.date("%Y-%m-%d %H:%M:%S", secs),
-            math.floor(ms / 1000),
-            M.LogLevelNames[level],
-            line
-          )
-        )
-      end
-      fp:close()
-    end
-  end
+--- @param opts fzfx.Options?
+M.setup = function(opts)
+  local configs =
+    vim.tbl_deep_extend("force", vim.deepcopy(Defaults), opts or {})
+  require("fzfx.commons.logging").setup({
+    name = "fzfx",
+    console_log = configs.console_log,
+    file_log = configs.file_log,
+    file_log_name = "fzfx.log",
+  })
 end
 
 --- @param fmt string
 --- @param ... any
 M.debug = function(fmt, ...)
-  _log(M.LogLevels.DEBUG, string.format(fmt, ...))
+  local dbglvl = 2
+  local dbg = nil
+  while true do
+    dbg = debug.getinfo(dbglvl, "nfSl")
+    if not dbg or dbg.what ~= "C" then
+      break
+    end
+    dbglvl = dbglvl + 1
+  end
+  require("fzfx.commons.logging")
+    .get("fzfx")
+    :_log(dbg, M.LogLevels.DEBUG, fmt, ...)
 end
 
 --- @param fmt string
 --- @param ... any
 M.info = function(fmt, ...)
-  _log(M.LogLevels.INFO, string.format(fmt, ...))
+  local dbglvl = 2
+  local dbg = nil
+  while true do
+    dbg = debug.getinfo(dbglvl, "nfSl")
+    if not dbg or dbg.what ~= "C" then
+      break
+    end
+    dbglvl = dbglvl + 1
+  end
+  require("fzfx.commons.logging")
+    .get("fzfx")
+    :_log(dbg, M.LogLevels.INFO, fmt, ...)
 end
 
 --- @param fmt string
 --- @param ... any
 M.warn = function(fmt, ...)
-  _log(M.LogLevels.WARN, string.format(fmt, ...))
+  local dbglvl = 2
+  local dbg = nil
+  while true do
+    dbg = debug.getinfo(dbglvl, "nfSl")
+    if not dbg or dbg.what ~= "C" then
+      break
+    end
+    dbglvl = dbglvl + 1
+  end
+  require("fzfx.commons.logging")
+    .get("fzfx")
+    :_log(dbg, M.LogLevels.WARN, fmt, ...)
 end
 
 --- @param fmt string
 --- @param ... any
 M.err = function(fmt, ...)
-  _log(M.LogLevels.ERROR, string.format(fmt, ...))
+  local dbglvl = 2
+  local dbg = nil
+  while true do
+    dbg = debug.getinfo(dbglvl, "nfSl")
+    if not dbg or dbg.what ~= "C" then
+      break
+    end
+    dbglvl = dbglvl + 1
+  end
+  require("fzfx.commons.logging")
+    .get("fzfx")
+    :_log(dbg, M.LogLevels.ERROR, fmt, ...)
 end
 
 --- @param fmt string
 --- @param ... any
 M.throw = function(fmt, ...)
-  M.err(fmt, ...)
+  local dbglvl = 2
+  local dbg = nil
+  while true do
+    dbg = debug.getinfo(dbglvl, "nfSl")
+    if not dbg or dbg.what ~= "C" then
+      break
+    end
+    dbglvl = dbglvl + 1
+  end
+  require("fzfx.commons.logging")
+    .get("fzfx")
+    :_log(dbg, M.LogLevels.ERROR, fmt, ...)
   error(string.format(fmt, ...))
 end
 
@@ -167,9 +155,21 @@ end
 --- @param fmt string
 --- @param ... any
 M.ensure = function(cond, fmt, ...)
-  if not cond then
-    M.throw(fmt, ...)
+  local dbglvl = 2
+  local dbg = nil
+  while true do
+    dbg = debug.getinfo(dbglvl, "nfSl")
+    if not dbg or dbg.what ~= "C" then
+      break
+    end
+    dbglvl = dbglvl + 1
   end
+  if not cond then
+    require("fzfx.commons.logging")
+      .get("fzfx")
+      :_log(dbg, M.LogLevels.ERROR, fmt, ...)
+  end
+  assert(cond, string.format(fmt, ...))
 end
 
 return M
