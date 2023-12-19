949520bcd4139f8e9706430c05aff7711559ab6c
linrongbin16 <linrongbin16@outlook.com>
2023-12-19 14:08:16 +0800

    chore

--- lua/fzfx/detail/popup.lua
+++ lua/fzfx/detail/popup.lua
@@ -480,10 +480,16 @@ local function resize_all_popup_window_instances()
 end
 
 local function setup()
-  vim.api.nvim_create_autocmd({ "WinResized", "VimResized" }, {
+  vim.api.nvim_create_autocmd({ "WinResized" }, {
     pattern = { "*" },
     callback = resize_all_popup_window_instances,
   })
+  if vim.fn.has("nvim-0.9") > 0 then
+    vim.api.nvim_create_autocmd({ "VimResized" }, {
+      pattern = { "*" },
+      callback = resize_all_popup_window_instances,
+    })
+  end
 end
 
 local M = {
