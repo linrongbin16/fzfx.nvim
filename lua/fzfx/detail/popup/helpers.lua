5d9e36ebc728b276a9429e3ad20c35a565797fb1
linrongbin16 <linrongbin16@outlook.com>
2024-01-11 15:49:09 +0800

    chore

---
+++ lua/fzfx/detail/popup/helpers.lua
@@ -0,0 +1,145 @@
+local numbers = require("fzfx.commons.numbers")
+local apis = require("fzfx.commons.apis")
+local fileios = require("fzfx.commons.fileios")
+
+local constants = require("fzfx.lib.constants")
+local log = require("fzfx.lib.log")
+local fzf_helpers = require("fzfx.detail.fzf_helpers")
+
+local M = {}
+
+-- WindowOptsContext {
+
+--- @class fzfx.WindowOptsContext
+--- @field bufnr integer
+--- @field tabnr integer
+--- @field winnr integer
+local WindowOptsContext = {}
+
+--- @return fzfx.WindowOptsContext
+function WindowOptsContext:save()
+  local o = {
+    bufnr = vim.api.nvim_get_current_buf(),
+    winnr = vim.api.nvim_get_current_win(),
+    tabnr = vim.api.nvim_get_current_tabpage(),
+  }
+  setmetatable(o, self)
+  self.__index = self
+  return o
+end
+
+function WindowOptsContext:restore()
+  if vim.api.nvim_tabpage_is_valid(self.tabnr) then
+    vim.api.nvim_set_current_tabpage(self.tabnr)
+  end
+  if vim.api.nvim_win_is_valid(self.winnr) then
+    vim.api.nvim_set_current_win(self.winnr)
+  end
+end
+
+M.WindowOptsContext = WindowOptsContext
+
+-- WindowOptsContext }
+
+-- ShellOptsContext {
+
+--- @class fzfx.ShellOptsContext
+--- @field shell string?
+--- @field shellslash string?
+--- @field shellcmdflag string?
+--- @field shellxquote string?
+--- @field shellquote string?
+--- @field shellredir string?
+--- @field shellpipe string?
+--- @field shellxescape string?
+local ShellOptsContext = {}
+
+--- @return fzfx.ShellOptsContext
+function ShellOptsContext:save()
+  local o = constants.IS_WINDOWS
+      and {
+        shell = vim.o.shell,
+        shellslash = vim.o.shellslash,
+        shellcmdflag = vim.o.shellcmdflag,
+        shellxquote = vim.o.shellxquote,
+        shellquote = vim.o.shellquote,
+        shellredir = vim.o.shellredir,
+        shellpipe = vim.o.shellpipe,
+        shellxescape = vim.o.shellxescape,
+      }
+    or {
+      shell = vim.o.shell,
+    }
+  setmetatable(o, self)
+  self.__index = self
+
+  if constants.IS_WINDOWS then
+    vim.o.shell = "cmd.exe"
+    vim.o.shellslash = false
+    vim.o.shellcmdflag = "/s /c"
+    vim.o.shellxquote = '"'
+    vim.o.shellquote = ""
+    vim.o.shellredir = ">%s 2>&1"
+    vim.o.shellpipe = "2>&1| tee"
+    vim.o.shellxescape = ""
+  else
+    vim.o.shell = "sh"
+  end
+
+  return o
+end
+
+function ShellOptsContext:restore()
+  if constants.IS_WINDOWS then
+    vim.o.shell = self.shell
+    vim.o.shellslash = self.shellslash
+    vim.o.shellcmdflag = self.shellcmdflag
+    vim.o.shellxquote = self.shellxquote
+    vim.o.shellquote = self.shellquote
+    vim.o.shellredir = self.shellredir
+    vim.o.shellpipe = self.shellpipe
+    vim.o.shellxescape = self.shellxescape
+  else
+    vim.o.shell = self.shell
+  end
+end
+
+M.ShellOptsContext = ShellOptsContext
+
+-- ShellOptsContext }
+
+--- @package
+--- @param value number
+--- @param size integer
+--- @return integer
+M.get_window_size = function(value, size)
+  return numbers.bound(value > 1 and value or math.floor(size * value), 3, size)
+end
+
+--- @param total_size integer
+--- @param size integer
+--- @param shift number
+--- @param additional_offset integer?
+--- @return number
+M.shift_window_pos = function(total_size, size, shift, additional_offset)
+  additional_offset = additional_offset or 0
+  local left_size = total_size - size
+  local half_left_size = math.floor(left_size * 0.5)
+  if shift >= 0 then
+    local offset = shift < 1 and math.floor(left_size * shift) or shift
+    return numbers.bound(
+      half_left_size + offset + additional_offset,
+      0,
+      left_size
+    )
+  else
+    local offset = shift > -1 and math.floor(left_size * shift) or shift
+    return numbers.bound(
+      half_left_size + offset + additional_offset,
+      0,
+      left_size
+    )
+  end
+end
+
+return M
