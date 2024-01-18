7d8dd7feb18540e00161848589a990c74102b950
linrongbin16 <linrongbin16@outlook.com>
2024-01-18 14:51:55 +0800

    chore

--- lua/fzfx/detail/bat_helpers.lua
+++ lua/fzfx/detail/bat_helpers.lua
@@ -207,6 +207,288 @@ M.get_custom_theme = function()
     strings.not_empty(line_nr.fg) and line_nr.fg
       or DEFAULT_BASE16_COLORS.GUTTER_FOREGROUND
   )
+  payload = payload:gsub(
+    "{TEXT_FOREGROUND}",
+    strings.not_empty(normal.fg) and normal.fg
+      or DEFAULT_BASE16_COLORS.TEXT_FOREGROUND
+  )
+  local comment = termcolors.retrieve("Comment")
+  payload = payload:gsub(
+    "{COMMENT_FOREGROUND}",
+    strings.not_empty(comment.fg) and comment.fg
+      or DEFAULT_BASE16_COLORS.COMMENT_FOREGROUND
+  )
+  payload = payload:gsub(
+    "{PUNCTUATION_FOREGROUND}",
+    strings.not_empty(normal.fg) and normal.fg
+      or DEFAULT_BASE16_COLORS.PUNCTUATION_FOREGROUND
+  )
+  local delimiter = termcolors.retrieve("Delimiter")
+  payload = payload:gsub(
+    "{DELIMITERS_FOREGROUND}",
+    strings.not_empty(delimiter.fg) and delimiter.fg
+      or DEFAULT_BASE16_COLORS.DELIMITERS_FOREGROUND
+  )
+  local operator = termcolors.retrieve("Operator")
+  payload = payload:gsub(
+    "{OPERATORS_FOREGROUND}",
+    strings.not_empty(operator.fg) and operator.fg
+      or DEFAULT_BASE16_COLORS.OPERATORS_FOREGROUND
+  )
+  local keyword = termcolors.retrieve("Keyword")
+  payload = payload:gsub(
+    "{KEYWORDS_FOREGROUND}",
+    strings.not_empty(keyword.fg) and keyword.fg
+      or DEFAULT_BASE16_COLORS.KEYWORDS_FOREGROUND
+  )
+  local identifier = termcolors.retrieve("Identifier")
+  payload = payload:gsub(
+    "{VARIABLES_FOREGROUND}",
+    strings.not_empty(identifier.fg) and identifier.fg
+      or DEFAULT_BASE16_COLORS.VARIABLES_FOREGROUND
+  )
+  local function1 = termcolors.retrieve("Function")
+  payload = payload:gsub(
+    "{FUNCTIONS_FOREGROUND}",
+    strings.not_empty(function1.fg) and function1.fg
+      or DEFAULT_BASE16_COLORS.FUNCTIONS_FOREGROUND
+  )
+  local label = termcolors.retrieve("Label")
+  payload = payload:gsub(
+    "{LABELS_FOREGROUND}",
+    strings.not_empty(label.fg) and label.fg
+      or DEFAULT_BASE16_COLORS.LABELS_FOREGROUND
+  )
+  local structure = termcolors.retrieve("Structure")
+  payload = payload:gsub(
+    "{CLASSES_FOREGROUND}",
+    strings.not_empty(structure.fg) and structure.fg
+      or DEFAULT_BASE16_COLORS.CLASSES_FOREGROUND
+  )
+  local type1 = termcolors.retrieve("Type")
+  payload = payload:gsub(
+    "{META_CLASSES_FOREGROUND}",
+    strings.not_empty(type1.fg) and type1.fg
+      or DEFAULT_BASE16_COLORS.META_CLASSES_FOREGROUND
+  )
+  payload = payload:gsub(
+    "{METHODS_FOREGROUND}",
+    strings.not_empty(function1.fg) and function1.fg
+      or DEFAULT_BASE16_COLORS.METHODS_FOREGROUND
+  )
+  local storage_class = termcolors.retrieve("StorageClass")
+  payload = payload:gsub(
+    "{STORAGE_FOREGROUND}",
+    strings.not_empty(storage_class.fg) and storage_class.fg
+      or DEFAULT_BASE16_COLORS.STORAGE_FOREGROUND
+  )
+  payload = payload:gsub(
+    "{SUPPORT_FOREGROUND}",
+    strings.not_empty(function1.fg) and function1.fg
+      or DEFAULT_BASE16_COLORS.SUPPORT_FOREGROUND
+  )
+  local string1 = termcolors.retrieve("String")
+  payload = payload:gsub(
+    "{STRINGS_FOREGROUND}",
+    strings.not_empty(string1.fg) and string1.fg
+      or DEFAULT_BASE16_COLORS.STRINGS_FOREGROUND
+  )
+  local number1 = termcolors.retrieve("Number")
+  payload = payload:gsub(
+    "{INTEGERS_FOREGROUND}",
+    strings.not_empty(number1.fg) and number1.fg
+      or DEFAULT_BASE16_COLORS.INTEGERS_FOREGROUND
+  )
+  local float1 = termcolors.retrieve("Float")
+  payload = payload:gsub(
+    "{FLOATS_FOREGROUND}",
+    strings.not_empty(float1.fg) and float1.fg
+      or DEFAULT_BASE16_COLORS.FLOATS_FOREGROUND
+  )
+  local boolean1 = termcolors.retrieve("Boolean")
+  payload = payload:gsub(
+    "{BOOLEAN_FOREGROUND}",
+    strings.not_empty(boolean1.fg) and boolean1.fg
+      or DEFAULT_BASE16_COLORS.BOOLEAN_FOREGROUND
+  )
+  local constant1 = termcolors.retrieve("Constant")
+  payload = payload:gsub(
+    "{CONSTANTS_FOREGROUND}",
+    strings.not_empty(constant1.fg) and constant1.fg
+      or DEFAULT_BASE16_COLORS.CONSTANTS_FOREGROUND
+  )
+  local tag = termcolors.retrieve("Tag")
+  payload = payload:gsub(
+    "{TAGS_FOREGROUND}",
+    strings.not_empty(tag.fg) and tag.fg
+      or DEFAULT_BASE16_COLORS.TAGS_FOREGROUND
+  )
+  local macro = termcolors.retrieve("Macro")
+  payload = payload:gsub(
+    "{ATTRIBUTES_FOREGROUND}",
+    strings.not_empty(macro.fg) and macro.fg
+      or DEFAULT_BASE16_COLORS.ATTRIBUTES_FOREGROUND
+  )
+  payload = payload:gsub(
+    "{ATTRIBUTE_IDS_FOREGROUND}",
+    strings.not_empty(identifier.fg) and identifier.fg
+      or DEFAULT_BASE16_COLORS.ATTRIBUTE_IDS_FOREGROUND
+  )
+  payload = payload:gsub(
+    "{SELECTOR_FOREGROUND}",
+    strings.not_empty(visual.fg) and visual.fg
+      or DEFAULT_BASE16_COLORS.SELECTOR_FOREGROUND
+  )
+  payload = payload:gsub(
+    "{VALUES_FOREGROUND}",
+    strings.not_empty(constant1.fg) and constant1.fg
+      or DEFAULT_BASE16_COLORS.SELECTOR_FOREGROUND
+  )
+  local title = termcolors.retrieve("Title")
+  payload = payload:gsub(
+    "{HEADINGS_FOREGROUND}",
+    strings.not_empty(title.fg) and title.fg
+      or DEFAULT_BASE16_COLORS.HEADINGS_FOREGROUND
+  )
+  payload = payload:gsub(
+    "{UNITS_FOREGROUND}",
+    strings.not_empty(keyword.fg) and keyword.fg
+      or DEFAULT_BASE16_COLORS.UNITS_FOREGROUND
+  )
+  payload = payload:gsub(
+    "{BOLD_FOREGROUND}",
+    strings.not_empty(normal.fg) and normal.fg
+      or DEFAULT_BASE16_COLORS.BOLD_FOREGROUND
+  )
+  payload = payload:gsub(
+    "{ITALIC_FOREGROUND}",
+    strings.not_empty(normal.fg) and normal.fg
+      or DEFAULT_BASE16_COLORS.ITALIC_FOREGROUND
+  )
+  local preproc = termcolors.retrieve("PreProc")
+  payload = payload:gsub(
+    "{CODE_FOREGROUND}",
+    strings.not_empty(preproc.fg) and preproc.fg
+      or DEFAULT_BASE16_COLORS.CODE_FOREGROUND
+  )
+
+  local help_command = termcolors.retrieve("helpCommand")
+  payload = payload:gsub(
+    "{LINK_TEXT_FOREGROUND}",
+    strings.not_empty(help_command.fg) and help_command.fg
+      or DEFAULT_BASE16_COLORS.LINK_TEXT_FOREGROUND
+  )
+  local help_hyper = termcolors.retrieve("helpHyperTextJump")
+  payload = payload:gsub(
+    "{LINK_URL_FOREGROUND}",
+    strings.not_empty(help_hyper.fg) and help_hyper.fg
+      or DEFAULT_BASE16_COLORS.LINK_URL_FOREGROUND
+  )
+  local character = termcolors.retrieve("Character")
+  payload = payload:gsub(
+    "{QUOTES_FOREGROUND}",
+    strings.not_empty(character.fg) and character.fg
+      or DEFAULT_BASE16_COLORS.QUOTES_FOREGROUND
+  )
+  local tabline = termcolors.retrieve("TabLine")
+  payload = payload:gsub(
+    "{SEPARATOR_BACKGROUND}",
+    strings.not_empty(tabline.bg) and tabline.bg
+      or DEFAULT_BASE16_COLORS.SEPARATOR_BACKGROUND
+  )
+  payload = payload:gsub(
+    "{SEPARATOR_FOREGROUND}",
+    strings.not_empty(tabline.fg) and tabline.fg
+      or DEFAULT_BASE16_COLORS.SEPARATOR_FOREGROUND
+  )
+  local diff_add = termcolors.retrieve("DiffAdd")
+  local diff_added = termcolors.retrieve("DiffAdded")
+  payload = payload:gsub(
+    "{INSERTED_FOREGROUND}",
+    strings.not_empty(diff_add.fg) and diff_add.fg
+      or (
+        strings.not_empty(diff_added.fg) and diff_added.fg
+        or DEFAULT_BASE16_COLORS.INSERTED_FOREGROUND
+      )
+  )
+  local diff_delete = termcolors.retrieve("DiffDelete")
+  local diff_removed = termcolors.retrieve("DiffRemoved")
+  payload = payload:gsub(
+    "{DELETED_FOREGROUND}",
+    strings.not_empty(diff_delete.fg) and diff_delete.fg
+      or (
+        strings.not_empty(diff_removed).fg and diff_removed.fg
+        or DEFAULT_BASE16_COLORS.DELETED_FOREGROUND
+      )
+  )
+  local diff_change = termcolors.retrieve("DiffChange")
+  payload = payload:gsub(
+    "{CHANGED_FOREGROUND}",
+    strings.not_empty(diff_change.fg) and diff_change.fg
+      or DEFAULT_BASE16_COLORS.CHANGED_FOREGROUND
+  )
+  local color_column = termcolors.retrieve("ColorColumn")
+  payload = payload:gsub(
+    "{COLORS_FOREGROUND}",
+    strings.not_empty(color_column.fg) and color_column.fg
+      or DEFAULT_BASE16_COLORS.COLORS_FOREGROUND
+  )
+  payload = payload:gsub(
+    "{REGULAR_EXPRESSIONS_FOREGROUND}",
+    strings.not_empty(string1.fg) and string1.fg
+      or DEFAULT_BASE16_COLORS.REGULAR_EXPRESSIONS_FOREGROUND
+  )
+  payload = payload:gsub(
+    "{ESCAPE_CHARACTERS_FOREGROUND}",
+    strings.not_empty(character.fg) and character.fg
+      or DEFAULT_BASE16_COLORS.ESCAPE_CHARACTERS_FOREGROUND
+  )
+  payload = payload:gsub(
+    "{EMBEDDED_FOREGROUND}",
+    strings.not_empty(normal.fg) and normal.fg
+      or DEFAULT_BASE16_COLORS.EMBEDDED_FOREGROUND
+  )
+  local exception = termcolors.retrieve("Exception")
+  payload = payload:gsub(
+    "{ILLEGAL_BACKGROUND}",
+    strings.not_empty(exception.bg) and exception.bg
+      or DEFAULT_BASE16_COLORS.ILLEGAL_BACKGROUND
+  )
+  payload = payload:gsub(
+    "{ILLEGAL_FOREGROUND}",
+    strings.not_empty(exception.fg) and exception.fg
+      or DEFAULT_BASE16_COLORS.ILLEGAL_FOREGROUND
+  )
+  payload = payload:gsub(
+    "{BROKEN_BACKGROUND}",
+    strings.not_empty(exception.bg) and exception.bg
+      or DEFAULT_BASE16_COLORS.BROKEN_BACKGROUND
+  )
+  payload = payload:gsub(
+    "{BROKEN_FOREGROUND}",
+    strings.not_empty(exception.fg) and exception.fg
+      or DEFAULT_BASE16_COLORS.BROKEN_FOREGROUND
+  )
+  payload = payload:gsub(
+    "{DEPRECATED_BACKGROUND}",
+    strings.not_empty(comment.fg) and comment.fg
+      or DEFAULT_BASE16_COLORS.DEPRECATED_BACKGROUND
+  )
+  payload = payload:gsub(
+    "{DEPRECATED_FOREGROUND}",
+    strings.not_empty(comment.fg) and comment.fg
+      or DEFAULT_BASE16_COLORS.DEPRECATED_FOREGROUND
+  )
+  payload = payload:gsub(
+    "{UNIMPLEMENTED_BACKGROUND}",
+    strings.not_empty(comment.bg) and comment.bg
+      or DEFAULT_BASE16_COLORS.UNIMPLEMENTED_BACKGROUND
+  )
+  payload = payload:gsub(
+    "{UNIMPLEMENTED_FOREGROUND}",
+    strings.not_empty(comment.fg) and comment.fg
+      or DEFAULT_BASE16_COLORS.UNIMPLEMENTED_FOREGROUND
+  )
 
   return {
     name = name,
