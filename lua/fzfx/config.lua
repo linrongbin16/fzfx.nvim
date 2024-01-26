b0e671a37ffe49fff7fb8aef3d0bb615934d88ac
linrongbin16 <linrongbin16@outlook.com>
2024-01-26 11:42:51 +0800

    chore

--- lua/fzfx/config.lua
+++ lua/fzfx/config.lua
@@ -125,22 +125,28 @@ local Defaults = {
   },
 
   buffer_previewer_opts = {
-    --- @alias fzfx.BufferFilePreviewerContext {previewer:{bufnr:integer,winnr:integer}?,provider:{bufnr:integer,winnr:integer}?}
-
     -- alt-p:toggle-preview
     --
-    --- @param context fzfx.BufferFilePreviewerContext
-    ["<A-p>"] = function(context) end,
+    --- @param buffer_popup_win fzfx.BufferPopupWindow
+    ["<A-p>"] = function(buffer_popup_win)
+      local current_win_confs =
+        vim.api.nvim_win_get_config(buffer_popup_win.previewer_winnr)
+      -- if already hide, show it
+      if current_win_confs.hide then
+      else
+        -- if not hide, hide it
+      end
+    end,
 
     -- ctrl-f:preview-half-page-down
     --
-    --- @param context fzfx.BufferFilePreviewerContext
-    ["<C-f>"] = function(context) end,
+    --- @param buffer_popup_win fzfx.BufferPopupWindow
+    ["<C-f>"] = function(buffer_popup_win) end,
 
     -- ctrl-b:preview-half-page-up
     --
-    --- @param context fzfx.BufferFilePreviewerContext
-    ["<C-b>"] = function(context) end,
+    --- @param buffer_popup_win fzfx.BufferPopupWindow
+    ["<C-b>"] = function(buffer_popup_win) end,
   },
 
   -- global fzf opts with highest priority.
