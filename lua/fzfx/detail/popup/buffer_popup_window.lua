f74438666ef1a24b5e7ee48cb1ddcb81db6da7f4
linrongbin16 <linrongbin16@outlook.com>
2024-01-30 17:45:24 +0800

    chore

--- lua/fzfx/detail/popup/buffer_popup_window.lua
+++ lua/fzfx/detail/popup/buffer_popup_window.lua
@@ -375,7 +375,8 @@ end
 local function _set_default_previewer_win_options(winnr)
   apis.set_win_option(winnr, "number", true)
   apis.set_win_option(winnr, "spell", false)
-  apis.set_win_option(winnr, "winhighlight", "Pmenu:,Normal:Normal")
+  -- apis.set_win_option(winnr, "winhighlight", "Pmenu:,Normal:Normal")
+  apis.set_win_option(winnr, "wrap", false)
 end
 
 local function _set_default_provider_win_options(winnr)
