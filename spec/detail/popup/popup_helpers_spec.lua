f1a6c8cfec28642fe845e06731a14e8d13556fd4
linrongbin16 <linrongbin16@outlook.com>
2024-02-18 13:48:18 +0800

    chore

--- spec/detail/popup/popup_helpers_spec.lua
+++ spec/detail/popup/popup_helpers_spec.lua
@@ -8,11 +8,18 @@ describe("detail.popup.popup_helpers", function()
   before_each(function()
     vim.api.nvim_command("cd " .. cwd)
     vim.opt.swapfile = false
+    vim.cmd([[edit README.md]])
   end)
 
   local tables = require("fzfx.commons.tables")
   local strings = require("fzfx.commons.strings")
   local popup_helpers = require("fzfx.detail.popup.popup_helpers")
+  require("fzfx").setup({
+    debug = {
+      enable = true,
+      file_log = true,
+    },
+  })
 
   describe("[WindowOptsContext]", function()
     it("save", function()
@@ -38,11 +45,21 @@ describe("detail.popup.popup_helpers", function()
   end)
   describe("[make_layout]", function()
     it("test without fzf_preview_window_opts", function()
-      local actual1 = popup_helpers.make_layout({ height = 0.75, width = 0.85, row = 0, col = 0 })
+      local actual1 = popup_helpers.make_layout({
+        relative = "editor",
+        height = 0.75,
+        width = 0.85,
+        row = 0,
+        col = 0,
+      })
+      print(string.format("make_layout-1:%s\n", vim.inspect(actual1)))
       assert_true(actual1.width >= math.floor(vim.o.columns * 0.85))
       assert_true(actual1.width <= math.ceil(vim.o.columns * 0.85))
       assert_true(actual1.height >= math.floor(vim.o.lines * 0.75))
       assert_true(actual1.height <= math.ceil(vim.o.lines * 0.75))
+      local center_row = vim.o.lines / 2
+      local center_col = vim.o.columns / 2
+      assert_eq(center_row - actual1.start_row, )
     end)
     it("test with fzf_preview_window_opts", function() end)
   end)
