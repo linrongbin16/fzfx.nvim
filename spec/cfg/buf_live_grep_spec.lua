cb92357c041cc2adeccca0de976dc3f1e1724c54
linrongbin16 <linrongbin16@outlook.com>
2024-03-07 17:26:20 +0800

    chore

--- spec/cfg/buf_live_grep_spec.lua
+++ spec/cfg/buf_live_grep_spec.lua
@@ -1,6 +1,6 @@
 local cwd = vim.fn.getcwd()
 
-describe("cfg.buf_live_grep", function()
+describe("fzfx.cfg.buf_live_grep", function()
   local assert_eq = assert.is_equal
   local assert_true = assert.is_true
   local assert_false = assert.is_false
--- spec/cfg/buffers_spec.lua
+++ spec/cfg/buffers_spec.lua
@@ -1,7 +1,7 @@
 ---@diagnostic disable: undefined-field, unused-local, missing-fields, need-check-nil, param-type-mismatch, assign-type-mismatch
 local cwd = vim.fn.getcwd()
 
-describe("cfg.buffers", function()
+describe("fzfx.cfg.buffers", function()
   local assert_eq = assert.is_equal
   local assert_true = assert.is_true
   local assert_false = assert.is_false
