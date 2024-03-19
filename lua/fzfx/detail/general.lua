c6018b1e62a3ad09a1f756bf1a5a81e34596fc31
linrongbin16 <linrongbin16@outlook.com>
2024-03-19 09:51:24 +0800

    chore

--- lua/fzfx/detail/general.lua
+++ lua/fzfx/detail/general.lua
@@ -896,6 +896,30 @@ local function mock_buffer_previewer_fzf_opts(fzf_opts, fzf_action_file)
     }
 end
 
+--- @param fzf_opts fzfx.FzfOpt[]
+--- @return fzfx.FzfOpt[], string
+local function mock_fzf_border_opts(fzf_opts)
+  local new_fzf_opts = {}
+  local border_opts = fzf_helpers.FZF_DEFAULT_BORDER_OPTS
+  for _, o in ipairs(fzf_opts) do
+    local mocked = false
+    if type(o) == "table" and str.not_empty(o[1]) and str.startswith(o[1], "--border") then
+      border_opts = o[2]
+      mocked = true
+    elseif
+      str.not_empty(o) and str.startswith(o --[[@as string]], "--border")
+    then
+      border_opts = string.sub(o --[[@as string]], string.len("--border") + 2)
+      mocked = true
+    end
+    if not mocked then
+      table.insert(new_fzf_opts, o)
+    end
+  end
+
+  return new_fzf_opts, border_opts
+end
+
 --- @param name string
 --- @param query string
 --- @param bang boolean
