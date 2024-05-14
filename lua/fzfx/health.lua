b7bcca728bb197891cd04aef642282fb675970c8
linrongbin16 <linrongbin16@outlook.com>
2024-05-14 14:42:17 +0800

    chore

--- lua/fzfx/health.lua
+++ lua/fzfx/health.lua
@@ -3,39 +3,20 @@ local tbl = require("fzfx.commons.tbl")
 
 local M = {}
 
-M._files = function()
-  local execs = tbl.List:of()
+M._find = function()
   if consts.HAS_FD then
-    execs:push(consts.FD)
+    vim.health.ok(string.format("'%s' found", consts.FD))
   elseif consts.HAS_FIND then
-    execs:push(consts.FIND)
+    vim.health.ok(string.format("'%s' found", consts.FIND))
   else
-    vim.health.error(
-      string.format("'FzfxFiles' doesn't have providers, 'fd'/'find'/'gfind' not found")
-    )
+    vim.health.error("'fd'/'find'/'gfind' not found")
   end
-
-  if consts.HAS_BAT then
-    execs:push(consts.BAT)
-  elseif consts.HAS_CAT then
-    execs:push(consts.CAT)
-  else
-    vim.health.warn(string.format("'bat'/'cat' not found"))
-  end
-
-  execs = execs
-    :map(function(value, index)
-      return string.format("'%s'", value)
-    end)
-    :data()
-
-  vim.health.ok(string.format("'FzfxFiles' uses %s", table.concat(execs, ",")))
 end
 
 M.check = function()
   vim.health.start("fzfx")
 
-  M._files()
+  M._find()
 end
 
 return M
