65f76e61cafb3a812c9badc84a1106524a90424a
linrongbin16 <linrongbin16@outlook.com>
2024-06-17 09:29:05 +0800

    refactor(health): part 2 - checkhealth (#712)

--- lua/fzfx/health.lua
+++ lua/fzfx/health.lua
@@ -1,61 +1,66 @@
+---@diagnostic disable: redundant-parameter
+
 local consts = require("fzfx.lib.constants")
 local tbl = require("fzfx.commons.tbl")
 local str = require("fzfx.commons.str")
 
 local M = {}
 
-local EXEC_CONFIGS = {
+--- @alias fzfx.HealthCheckItem {cond:boolean,name:string,version:string?,line:integer?}
+--- @alias fzfx.HealthCheck {items:fzfx.HealthCheckItem[],missed:string[]}
+--- @type fzfx.HealthCheck[]
+M.HEALTH_CHECKS = {
   {
     items = {
       { cond = consts.HAS_FZF, name = consts.FZF, version = "--version", line = 1 },
     },
-    fail = { "fzf" },
+    missed = { "fzf" },
   },
   {
     items = {
       { cond = consts.HAS_ECHO, name = consts.ECHO },
     },
-    fail = { "echo" },
+    missed = { "echo" },
   },
   {
     items = {
       { cond = consts.HAS_CURL, name = consts.CURL, version = "--version", line = 1 },
     },
-    fail = { "curl" },
+    missed = { "curl" },
   },
   {
     items = {
       { cond = consts.HAS_FD, name = consts.FD, version = "--version", line = 1 },
       { cond = consts.HAS_FIND, name = consts.FIND },
     },
-    fail = { "fd", "find", "gfind" },
+    missed = { "fd", "find", "gfind" },
   },
   {
     items = {
       { cond = consts.HAS_BAT, name = consts.BAT, version = "--version", line = 1 },
       { cond = consts.HAS_CAT, name = consts.CAT },
     },
-    fail = { "bat", "batcat", "cat" },
+    missed = { "bat", "batcat", "cat" },
   },
   {
     items = {
       { cond = consts.HAS_RG, name = consts.RG, version = "--version", line = 1 },
       { cond = consts.HAS_GREP, name = consts.GREP },
     },
-    fail = { "rg", "grep", "ggrep" },
+    missed = { "rg", "grep", "ggrep" },
   },
   {
     items = {
       { cond = consts.HAS_GIT, name = consts.GIT, version = "--version", line = 1 },
     },
-    fail = { "git" },
+    missed = { "git" },
   },
   {
     items = {
       { cond = consts.HAS_DELTA, name = consts.DELTA, version = "--version", line = 1 },
     },
-    fail = { "delta" },
-    fail_level = "warn",
+    missed = { "delta" },
+    missed_level = "warn",
   },
   {
     items = {
@@ -63,85 +68,111 @@ local EXEC_CONFIGS = {
       { cond = consts.HAS_EZA, name = consts.EZA, version = "--version", line = 2 },
       { cond = consts.HAS_LS, name = consts.LS },
     },
-    fail = { "lsd", "eza", "exa", "ls" },
+    missed = { "lsd", "eza", "exa", "ls" },
   },
 }
+
+--- @param items commons.List
+--- @return string
+M._summary = function(items)
+  local n = items:length()
+  local founded_items = items
+    :map(function(
+      item --[[@as fzfx.HealthCheckItem]],
+      index
+    )
+      return string.format(
+        "'%s'%s",
+        vim.fn.fnamemodify(item.name, ":~:."),
+        (index == 1 and n > 1) and " (preferred)" or ""
+      )
+    end)
+    :join(", ")
+  return "Found " .. founded_items
+end
+
+--- @param items commons.List
+--- @return string
+M._versions = function(items)
+  local result = ""
+  items
+    :map(function(
+      item --[[@as fzfx.HealthCheckItem]]
+    )
+      if str.not_empty(item.version) then
+        local ok, output = pcall(vim.fn.systemlist, { item.name, item.version })
+        return {
+          ok = ok,
+          unversioned = false,
+          output = output,
+          line = item.line,
+          name = vim.fn.fnamemodify(item.name, ":~:."),
+        }
+      else
+        return {
+          ok = false,
+          unversioned = true,
+          output = nil,
+          line = item.line,
+          name = vim.fn.fnamemodify(item.name, ":~:."),
+        }
+      end
+    end)
+    :forEach(function(
+      ver --[[@as {ok:boolean,unversioned:boolean,output:string[]|nil,line:integer?,name:string}]]
+    )
+      if
+        tbl.tbl_not_empty(ver)
+        and ver.ok
+        and tbl.list_not_empty(ver.output)
+        and #ver.output >= ver.line
+      then
+        local target_line = nil
+        for i, out_line in ipairs(ver.output) do
+          if i == ver.line then
+            target_line = str.trim(out_line)
+            break
+          end
+        end
+        result = result .. string.format("\n  - '%s': %s", ver.name, target_line)
+      elseif tbl.tbl_not_empty(ver) and not ver.unversioned and not ver.ok then
+        result = result
+          .. string.format("\n  - (**Warning**) '%s': failed to get version info", ver.name)
+      end
+    end)
+  return result
+end
+
+--- @param misses string[]
+--- @return string
+M._misses = function(misses)
+  local result = tbl.List
+    :copy(misses)
+    :map(function(item)
+      return string.format("'%s'", item)
+    end)
+    :join(", ")
+  return "Missing " .. result
+end
 
 M.check = function()
   vim.health.start("fzfx")
 
-  for _, config in ipairs(EXEC_CONFIGS) do
-    local exec = tbl.List:of()
-    for _, item in ipairs(config.items) do
-      if item.cond then
-        exec:push(item)
-      end
-    end
-    if not exec:empty() then
-      local n = exec:length()
-      local all_exec = exec
-        :map(function(item, index)
-          return string.format(
-            "'%s'%s",
-            vim.fn.fnamemodify(item.name, ":~:."),
-            (index == 1 and n > 1) and " (preferred)" or ""
-          )
-        end)
-        :data()
-      local msg = string.format("Found %s", table.concat(all_exec, ", "))
-      local all_version = exec
-        :map(function(item, index)
-          if str.not_empty(item.version) then
-            local ok, output = pcall(vim.fn.systemlist, { item.name, item.version })
-            return {
-              ok = ok,
-              unversioned = false,
-              output = output,
-              line = item.line,
-              name = vim.fn.fnamemodify(item.name, ":~:."),
-            }
-          else
-            return {
-              ok = false,
-              unversioned = true,
-              output = nil,
-              line = item.line,
-              name = vim.fn.fnamemodify(item.name, ":~:."),
-            }
-          end
-        end)
-        :data()
-      for i, version in ipairs(all_version) do
-        if
-          tbl.tbl_not_empty(version)
-          and version.ok
-          and tbl.list_not_empty(version.output)
-          and #version.output >= version.line
-        then
-          local target_line = nil
-          for j, out_line in ipairs(version.output) do
-            if j == version.line then
-              target_line = str.trim(out_line)
-              break
-            end
-          end
-          msg = msg .. string.format("\n  - '%s': %s", version.name, target_line)
-        elseif tbl.tbl_not_empty(version) and not version.unversioned and not version.ok then
-          msg = msg
-            .. string.format("\n  - (**Warning**) '%s': failed to get version info", version.name)
-        end
-      end
+  for _, config in ipairs(M.HEALTH_CHECKS) do
+    local configured_items = tbl.List:copy(config.items)
+    local items = configured_items:filter(function(
+      item --[[@as fzfx.HealthCheckItem]]
+    )
+      return item.cond
+    end)
+
+    if not items:empty() then
+      local msg = M._summary(items)
+      msg = msg .. M._versions(items)
       vim.health.ok(msg)
     else
-      local all_exec = tbl.List
-        :copy(config.fail)
-        :map(function(item)
-          return string.format("'%s'", item)
-        end)
-        :data()
-      local fail_level = config.fail_level or "error"
-      local fail_message = vim.health[fail_level]
-      fail_message(string.format("Missing %s", table.concat(all_exec, ", ")))
+      local msg = M._misses(config.missed)
+      vim.health.error(msg)
     end
   end
 end
