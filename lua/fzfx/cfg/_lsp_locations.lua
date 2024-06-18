2f74e8208ef834579cde691392ce7d7e78926cc8
linrongbin16 <linrongbin16@outlook.com>
2024-06-18 11:35:35 +0800

    chore

--- lua/fzfx/cfg/_lsp_locations.lua
+++ lua/fzfx/cfg/_lsp_locations.lua
@@ -396,7 +396,7 @@ M._make_lsp_call_hierarchy_provider = function(opts)
   --- @return string[]|nil
   local function impl(query, context)
     ---@diagnostic disable-next-line: deprecated
-    local lsp_clients = vim.lsp.get_active_clients({ bufnr = context.bufnr })
+    local lsp_clients = lsp.get_clients({ bufnr = context.bufnr })
     if tbl.tbl_empty(lsp_clients) then
       log.echo(LogLevels.INFO, "no active lsp clients.")
       return nil
