6ed0ebe1add5a075f2f87321e37dbc049bdb0f86
linrongbin16 <linrongbin16@outlook.com>
2024-06-18 14:47:52 +0800

    chore

--- spec/cfg/git_live_grep_spec.lua
+++ spec/cfg/git_live_grep_spec.lua
@@ -12,7 +12,7 @@ describe("fzfx.cfg.git_live_grep", function()
     vim.cmd([[noautocmd edit README.md]])
   end)
 
-  local github_actions = os.getenv("GITHUB_ACTIONS") == "true"
+  local GITHUB_ACTIONS = os.getenv("GITHUB_ACTIONS") == "true"
 
   local tbl = require("fzfx.commons.tbl")
   local constants = require("fzfx.lib.constants")
@@ -26,50 +26,50 @@ describe("fzfx.cfg.git_live_grep", function()
     it("case-1: without extra options", function()
       local actual = git_live_grep_cfg._provider("", {})
       print(string.format("_provider-1:%s\n", vim.inspect(actual)))
-      if actual ~= nil then
-        assert_eq(type(actual), "table")
-        local n = #git_live_grep_cfg._GIT_GREP
-        for i = 1, n do
-          assert_eq(actual[i], git_live_grep_cfg._GIT_GREP[i])
-        end
+      -- if actual ~= nil then
+      assert_eq(type(actual), "table")
+      local n = #git_live_grep_cfg._GIT_GREP
+      for i = 1, n do
+        assert_eq(actual[i], git_live_grep_cfg._GIT_GREP[i])
       end
+      -- end
     end)
     it("case-2: with 'fzfx -- -v'", function()
       local actual = git_live_grep_cfg._provider("fzfx -- -v", {})
       print(string.format("_provider-2:%s\n", vim.inspect(actual)))
-      if actual ~= nil then
-        assert_eq(type(actual), "table")
-        local n = #git_live_grep_cfg._GIT_GREP
-        for i = 1, n do
-          assert_eq(actual[i], git_live_grep_cfg._GIT_GREP[i])
-        end
-        assert_true(tbl.List:move(actual):some(function(a)
-          return a == "fzfx"
-        end))
-        assert_true(tbl.List:move(actual):some(function(a)
-          return a == "-v"
-        end))
+      -- if actual ~= nil then
+      assert_eq(type(actual), "table")
+      local n = #git_live_grep_cfg._GIT_GREP
+      for i = 1, n do
+        assert_eq(actual[i], git_live_grep_cfg._GIT_GREP[i])
       end
+      assert_true(tbl.List:move(actual):some(function(a)
+        return a == "fzfx"
+      end))
+      assert_true(tbl.List:move(actual):some(function(a)
+        return a == "-v"
+      end))
+      -- end
     end)
     it("case-3: with 'fzfx -- -v -E  ' options", function()
       local actual = git_live_grep_cfg._provider("fzfx -- -v  -E  ", {})
       print(string.format("_provider-3:%s\n", vim.inspect(actual)))
-      if actual ~= nil then
-        assert_eq(type(actual), "table")
-        local n = #git_live_grep_cfg._GIT_GREP
-        for i = 1, n do
-          assert_eq(actual[i], git_live_grep_cfg._GIT_GREP[i])
-        end
-        assert_true(tbl.List:move(actual):some(function(a)
-          return a == "fzfx"
-        end))
-        assert_true(tbl.List:move(actual):some(function(a)
-          return a == "-v"
-        end))
-        assert_true(tbl.List:move(actual):some(function(a)
-          return a == "-E"
-        end))
+      -- if actual ~= nil then
+      assert_eq(type(actual), "table")
+      local n = #git_live_grep_cfg._GIT_GREP
+      for i = 1, n do
+        assert_eq(actual[i], git_live_grep_cfg._GIT_GREP[i])
       end
+      assert_true(tbl.List:move(actual):some(function(a)
+        return a == "fzfx"
+      end))
+      assert_true(tbl.List:move(actual):some(function(a)
+        return a == "-v"
+      end))
+      assert_true(tbl.List:move(actual):some(function(a)
+        return a == "-E"
+      end))
+      -- end
     end)
   end)
 end)
