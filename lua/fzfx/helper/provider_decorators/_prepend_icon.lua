9cea49821e17eacd01996b7639e991049714eb24
linrongbin16 <linrongbin16@outlook.com>
2024-06-20 13:34:41 +0800

    chore

--- lua/fzfx/helper/provider_decorators/_prepend_icon.lua
+++ lua/fzfx/helper/provider_decorators/_prepend_icon.lua
@@ -3,6 +3,7 @@
 -- Such as `FzfxFiles`, `FzfxLiveGrep`, `FzfxBuffers`, `FzfxGLiveGrep`, `FzfxLspDiagnostics` etc.
 
 local str = require("fzfx.commons.str")
+local path = require("fzfx.commons.path")
 local color_term = require("fzfx.commons.color.term")
 
 local DEVICONS = nil
@@ -25,32 +26,43 @@ M._decorate = function(line, delimiter, index)
     return line
   end
 
-  local filename = nil
+  --- @type string
+  local filename
   if str.not_empty(delimiter) and type(index) == "number" then
+    -- If specified the `delimiter` and `index`, then the `filename` is only part of the `line`.
+    -- Need to first split by `delimiter`, then `filename` is the `index`-th part.
     local splits = str.split(line, delimiter --[[@as string]])
     filename = splits[index]
   else
     filename = line
   end
-  -- remove ansi color codes
-  -- see: https://stackoverflow.com/a/55324681/4438921
+
+  -- remove (terminal) ansi color codes
   if str.not_empty(filename) then
-    filename = color_term.erase(filename)
+    filename = color_term.erase(filename) --[[@as string]]
   end
-  local ext = vim.fn.fnamemodify(filename --[[@as string]], ":e")
-  local icon_text, icon_color = DEVICONS.get_icon_color(filename, ext)
-  -- log_debug(
-  --     "|fzfx.shell_helpers - render_line_with_icon| ext:%s, icon:%s, icon_color:%s",
-  --     vim.inspect(ext),
-  --     vim.inspect(icon),
-  --     vim.inspect(icon_color)
-  -- )
-  if str.not_empty(icon_text) then
-    local rendered_text = color_term.render(icon_text, icon_color)
-    return rendered_text .. " " .. line
+
+  if path.isdir(filename) then
+    -- If `filename` is a directory
+    return string.format("%s %s", vim.env._FZFX_NVIM_FILE_FOLDER_ICON, line)
   else
-    if vim.fn.isdirectory(filename) > 0 then
-      return string.format("%s %s", vim.env._FZFX_NVIM_FILE_FOLDER_ICON, line)
+    -- Try to query file type icon and color for it.
+    local text, color = DEVICONS.get_icon_color(filename, nil, { default = true })
+
+    -- log_debug(
+    --     "|fzfx.shell_helpers - render_line_with_icon| ext:%s, icon:%s, icon_color:%s",
+    --     vim.inspect(ext),
+    --     vim.inspect(icon),
+    --     vim.inspect(icon_color)
+    -- )
+
+    if str.not_empty(text) then
+      if str.not_empty(color) and string.len(color) == 7 and str.startswith(color, "#") then
+        local rendered_text = color_term.render(text, color)
+        return rendered_text .. " " .. line
+      else
+        return text .. " " .. line
+      end
     else
       return string.format("%s %s", vim.env._FZFX_NVIM_UNKNOWN_FILE_ICON, line)
     end
