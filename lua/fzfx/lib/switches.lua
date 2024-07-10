9d7a5d7775d2204838521e7afb53f6a8d63979df
linrongbin16 <linrongbin16@outlook.com>
2024-07-10 09:19:34 +0800

    chore

--- lua/fzfx/lib/switches.lua
+++ lua/fzfx/lib/switches.lua
@@ -1,14 +1,18 @@
 local M = {}
 
+-- Detect whether the global `fzfx_disable_buffer_previewer` variable is been set.
+-- Returns `true` only if the variable is a positive number, or a `true` boolean.
+-- Otherwise returns `false`.
 --- @return boolean
 M.buffer_previewer_disabled = function()
-  return (
-    type(vim.g.fzfx_disable_buffer_previewer) == "number"
-    and vim.g.fzfx_disable_buffer_previewer > 0
-  )
-    or (
-      type(vim.g.fzfx_disable_buffer_previewer) == "boolean" and vim.g.fzfx_disable_buffer_previewer
-    )
+  local v = vim.g.fzfx_disable_buffer_previewer
+  if type(v) == "number" and v > 0 then
+    return true
+  end
+  if type(v) == "boolean" then
+    return v
+  end
+  return false
 end
 
 return M
