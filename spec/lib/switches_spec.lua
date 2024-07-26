e4706f58fe2b8b457a96c672ff340645a4fe7928
linrongbin16 <linrongbin16@outlook.com>
2024-07-26 10:08:23 +0800

    chore

--- spec/lib/switches_spec.lua
+++ spec/lib/switches_spec.lua
@@ -10,26 +10,32 @@ describe("lib.switches", function()
   end)
 
   local switches = require("fzfx.lib.switches")
+  describe("[_convert_boolean]", function()
+    it("test", function()
+      assert_true(switches._convert_boolean(1))
+      assert_true(switches._convert_boolean(true))
+      assert_false(switches._convert_boolean("true"))
+      assert_false(switches._convert_boolean(nil))
+      assert_false(switches._convert_boolean())
+      assert_false(switches._convert_boolean(false))
+      assert_false(switches._convert_boolean("false"))
+      assert_false(switches._convert_boolean(0))
+    end)
+  end)
   describe("[fzfx_disable_buffer_previewer]", function()
-    it("disabled", function()
+    it("test", function()
       vim.g.fzfx_disable_buffer_previewer = 1
       assert_true(switches.buffer_previewer_disabled())
       vim.g.fzfx_disable_buffer_previewer = true
       assert_true(switches.buffer_previewer_disabled())
-      vim.g.fzfx_disable_buffer_previewer = "true"
-      assert_false(switches.buffer_previewer_disabled())
-      vim.g.fzfx_disable_buffer_previewer = nil
-      assert_false(switches.buffer_previewer_disabled())
     end)
-    it("enabled", function()
-      vim.g.fzfx_disable_buffer_previewer = 0
-      assert_false(switches.buffer_previewer_disabled())
-      vim.g.fzfx_disable_buffer_previewer = false
-      assert_false(switches.buffer_previewer_disabled())
-      vim.g.fzfx_disable_buffer_previewer = "false"
-      assert_false(switches.buffer_previewer_disabled())
-      vim.g.fzfx_disable_buffer_previewer = nil
-      assert_false(switches.buffer_previewer_disabled())
+  end)
+  describe("[fzfx_enable_bat_theme_autogen]", function()
+    it("test", function()
+      vim.g.fzfx_enable_bat_theme_autogen = 1
+      assert_true(switches.bat_theme_autogen_enabled())
+      vim.g.fzfx_enable_bat_theme_autogen = true
+      assert_true(switches.bat_theme_autogen_enabled())
     end)
   end)
 end)
