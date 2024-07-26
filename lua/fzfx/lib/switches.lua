b3da4f8918f9058fbb4a3c5904ad5f9b16ad51de
linrongbin16 <linrongbin16@outlook.com>
2024-07-26 10:05:52 +0800

    chore

--- lua/fzfx/lib/switches.lua
+++ lua/fzfx/lib/switches.lua
@@ -1,8 +1,8 @@
 local M = {}
 
+--- @param value any
 --- @return boolean
-M.buffer_previewer_disabled = function()
-  local value = vim.g.fzfx_disable_buffer_previewer
+M._convert_boolean = function(value)
   if type(value) == "number" then
     return value > 0
   end
@@ -13,15 +13,13 @@ M.buffer_previewer_disabled = function()
 end
 
 --- @return boolean
+M.buffer_previewer_disabled = function()
+  return M._convert_boolean(vim.g.fzfx_disable_buffer_previewer)
+end
+
+--- @return boolean
 M.bat_theme_autogen_enabled = function()
-  local value = vim.g.fzfx_enable_bat_theme_autogen
-  if type(value) == "number" then
-    return value > 0
-  end
-  if type(value) == "boolean" then
-    return value
-  end
-  return false
+  return M._convert_boolean(vim.g.fzfx_enable_bat_theme_autogen)
 end
 
 return M
