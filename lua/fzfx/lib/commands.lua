d0d7e6ae9edb48869107873caad8fc3e26aca4b0
linrongbin16 <linrongbin16@outlook.com>
2024-12-25 12:57:47 +0800

    chore

--- lua/fzfx/lib/commands.lua
+++ lua/fzfx/lib/commands.lua
@@ -173,6 +173,28 @@ M.GitRootCommand = GitRootCommand
 
 -- GitRootCommand }
 
+-- GitRootAsyncCommand {
+
+--- @class fzfx.GitRootAsyncCommand
+--- @field async_command fzfx.AsyncCommand?
+local GitRootAsyncCommand = {}
+
+--- @param on_complete fun(result: fzfx.CommandResult):nil
+--- @return fzfx.GitRootAsyncCommand
+function GitRootAsyncCommand:run(on_complete)
+  local acommand = AsyncCommand:run({ "git", "rev-parse", "--show-toplevel" }, on_complete)
+  local o = {
+    async_command = acommand,
+  }
+  setmetatable(o, self)
+  self.__index = self
+  return o
+end
+
+M.GitRootAsyncCommand = GitRootAsyncCommand
+
+-- GitRootAsyncCommand }
+
 -- GitBranchesCommand {
 
 --- @class fzfx.GitBranchesCommand
@@ -209,6 +231,31 @@ end
 M.GitBranchesCommand = GitBranchesCommand
 
 -- GitBranchesCommand }
+
+-- GitBranchesAsyncCommand {
+
+--- @class fzfx.GitBranchesAsyncCommand
+--- @field async_command fzfx.AsyncCommand?
+local GitBranchesAsyncCommand = {}
+
+--- @param remotes boolean?
+--- @param on_complete fun(result: fzfx.CommandResult):nil
+--- @return fzfx.GitBranchesAsyncCommand
+function GitBranchesAsyncCommand:run(remotes, on_complete)
+  local cmd = remotes and AsyncCommand:run({ "git", "branch", "--remotes" }, on_complete)
+    or AsyncCommand:run({ "git", "branch" }, on_complete)
+
+  local o = {
+    async_command = cmd,
+  }
+  setmetatable(o, self)
+  self.__index = self
+  return o
+end
+
+M.GitBranchesAsyncCommand = GitBranchesAsyncCommand
+
+-- GitBranchesAsyncCommand }
 
 -- GitCurrentBranchCommand {
 
@@ -247,6 +294,30 @@ M.GitCurrentBranchCommand = GitCurrentBranchCommand
 
 -- GitCurrentBranchCommand }
 
+-- GitCurrentBranchAsyncCommand {
+
+--- @class fzfx.GitCurrentBranchAsyncCommand
+--- @field async_command fzfx.AsyncCommand?
+local GitCurrentBranchAsyncCommand = {}
+
+--- @param on_complete fun(result: fzfx.CommandResult):nil
+--- @return fzfx.GitCurrentBranchAsyncCommand
+function GitCurrentBranchAsyncCommand:run(on_complete)
+  -- git rev-parse --abbrev-ref HEAD
+  local acommand = AsyncCommand:run({ "git", "rev-parse", "--abbrev-ref", "HEAD" }, on_complete)
+
+  local o = {
+    async_command = acommand,
+  }
+  setmetatable(o, self)
+  self.__index = self
+  return o
+end
+
+M.GitCurrentBranchAsyncCommand = GitCurrentBranchAsyncCommand
+
+-- GitCurrentBranchAsyncCommand }
+
 -- GitRemotesCommand {
 
 --- @class fzfx.GitRemotesCommand
@@ -283,5 +354,29 @@ end
 M.GitRemotesCommand = GitRemotesCommand
 
 -- GitRemotesCommand }
+
+-- GitRemotesAsyncCommand {
+
+--- @class fzfx.GitRemotesAsyncCommand
+--- @field async_command fzfx.AsyncCommand?
+local GitRemotesAsyncCommand = {}
+
+--- @param on_complete fun(result: fzfx.CommandResult):nil
+--- @return fzfx.GitRemotesAsyncCommand
+function GitRemotesAsyncCommand:run(on_complete)
+  -- git rev-parse --abbrev-ref HEAD
+  local acommand = AsyncCommand:run({ "git", "remote" }, on_complete)
+
+  local o = {
+    async_command = acommand,
+  }
+  setmetatable(o, self)
+  self.__index = self
+  return o
+end
+
+M.GitRemotesAsyncCommand = GitRemotesAsyncCommand
+
+-- GitRemotesAsyncCommand }
 
 return M
