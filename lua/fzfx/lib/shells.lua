336d854501f9ac2c315896a6ea9ba4d1a2ec816f
linrongbin16 <linrongbin16@outlook.com>
2025-01-16 09:28:05 +0800

    chore

--- lua/fzfx/lib/shells.lua
+++ lua/fzfx/lib/shells.lua
@@ -3,17 +3,16 @@ local constants = require("fzfx.lib.constants")
 local M = {}
 
 --- @param s string
---- @param special any?
 --- @return string
-M.shellescape = function(s, special)
+M.shellescape = function(s)
   if constants.IS_WINDOWS then
     local shellslash = vim.o.shellslash
     vim.o.shellslash = false
-    local result = special ~= nil and vim.fn.shellescape(s, special) or vim.fn.shellescape(s)
+    local result = vim.fn.shellescape(s)
     vim.o.shellslash = shellslash
     return result
   else
-    return special ~= nil and vim.fn.shellescape(s, special) or vim.fn.shellescape(s)
+    return vim.fn.shellescape(s)
   end
 end
 
