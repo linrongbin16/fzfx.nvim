adeea82274b3ac8d0af3ce11a05e3fbccc3f6e56
linrongbin16 <linrongbin16@outlook.com>
2025-01-16 09:16:40 +0800

    feat(shell): improve shell escaping

--- lua/fzfx/lib/shells.lua
+++ lua/fzfx/lib/shells.lua
@@ -17,4 +17,71 @@ M.shellescape = function(s, special)
   end
 end
 
+-- ShellContext {
+
+--- @class fzfx.ShellContext
+--- @field shell string?
+--- @field shellslash string?
+--- @field shellcmdflag string?
+--- @field shellxquote string?
+--- @field shellquote string?
+--- @field shellredir string?
+--- @field shellpipe string?
+--- @field shellxescape string?
+local ShellContext = {}
+
+--- @return fzfx.ShellContext
+function ShellContext:save()
+  local o = constants.IS_WINDOWS
+      and {
+        shell = vim.o.shell,
+        shellslash = vim.o.shellslash,
+        shellcmdflag = vim.o.shellcmdflag,
+        shellxquote = vim.o.shellxquote,
+        shellquote = vim.o.shellquote,
+        shellredir = vim.o.shellredir,
+        shellpipe = vim.o.shellpipe,
+        shellxescape = vim.o.shellxescape,
+      }
+    or {
+      shell = vim.o.shell,
+    }
+  setmetatable(o, self)
+  self.__index = self
+
+  if constants.IS_WINDOWS then
+    vim.o.shell = "cmd.exe"
+    vim.o.shellslash = false
+    vim.o.shellcmdflag = "/s /c"
+    vim.o.shellxquote = '"'
+    vim.o.shellquote = ""
+    vim.o.shellredir = ">%s 2>&1"
+    vim.o.shellpipe = "2>&1| tee"
+    vim.o.shellxescape = ""
+  else
+    vim.o.shell = "sh"
+  end
+
+  return o
+end
+
+function ShellContext:restore()
+  if constants.IS_WINDOWS then
+    vim.o.shell = self.shell
+    vim.o.shellslash = self.shellslash
+    vim.o.shellcmdflag = self.shellcmdflag
+    vim.o.shellxquote = self.shellxquote
+    vim.o.shellquote = self.shellquote
+    vim.o.shellredir = self.shellredir
+    vim.o.shellpipe = self.shellpipe
+    vim.o.shellxescape = self.shellxescape
+  else
+    vim.o.shell = self.shell
+  end
+end
+
+M.ShellContext = ShellContext
+
+-- ShellContext }
+
 return M
