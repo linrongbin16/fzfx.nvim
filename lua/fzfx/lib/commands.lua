eeee102f6ccfa4969820c1b07ee7e69d1bd8bbdd
linrongbin16 <linrongbin16@outlook.com>
2025-02-02 22:26:20 +0800

    chore

--- lua/fzfx/lib/commands.lua
+++ lua/fzfx/lib/commands.lua
@@ -42,11 +42,11 @@ M.CommandResult = CommandResult
 
 -- CommandResult }
 
--- Commands {
+-- Async Commands {
 
 --- @param source string[]
 --- @param on_complete fun(result:fzfx.CommandResult):any
-local function run_impl(source, on_complete)
+local function run_async_impl(source, on_complete)
   assert(type(on_complete) == "function", "The 2nd parameter must be callback function")
 
   local stdout_data = {}
@@ -71,41 +71,97 @@ end
 
 -- Convert callback-style function 'run_impl' into async-style.
 --- @type async fun(source:string[]):fzfx.CommandResult
-M.run = async.wrap(2, run_impl)
+M.run_async = async.wrap(2, run_async_impl)
 
 -- Get git repository root path.
 --- @type async fun():fzfx.CommandResult
-M.run_git_root = async.wrap(1, function(on_complete)
-  run_impl({ "git", "rev-parse", "--show-toplevel" }, on_complete)
+M.run_git_root_async = async.wrap(1, function(on_complete)
+  run_async_impl({ "git", "rev-parse", "--show-toplevel" }, on_complete)
 end)
 
 -- Get git repository branches.
 --- @type async fun():fzfx.CommandResult
-M.run_git_branches = async.wrap(
+M.run_git_branches_async = async.wrap(
   2,
   --- @param remote boolean?
   --- @param on_complete fun(result:fzfx.CommandResult):any
   function(remote, on_complete)
     if remote then
-      run_impl({ "git", "branch", "--remotes" }, on_complete)
+      run_async_impl({ "git", "branch", "--remotes" }, on_complete)
     else
-      run_impl({ "git", "branch" }, on_complete)
+      run_async_impl({ "git", "branch" }, on_complete)
     end
   end
 )
 
 -- Get git repository current branch.
 --- @type async fun():fzfx.CommandResult
-M.run_git_current_branch = async.wrap(1, function(on_complete)
-  run_impl({ "git", "rev-parse", "--abbrev-ref", "HEAD" }, on_complete)
+M.run_git_current_branch_async = async.wrap(1, function(on_complete)
+  run_async_impl({ "git", "rev-parse", "--abbrev-ref", "HEAD" }, on_complete)
 end)
 
 -- Get git repository remotes.
 --- @type async fun():fzfx.CommandResult
-M.run_git_remotes = async.wrap(1, function(on_complete)
-  run_impl({ "git", "remote" }, on_complete)
+M.run_git_remotes_async = async.wrap(1, function(on_complete)
+  run_async_impl({ "git", "remote" }, on_complete)
 end)
 
--- Commands }
+-- Async Commands }
+
+-- Sync Commands {
+
+--- @param source string[]
+--- @return fzfx.CommandResult
+M.run_sync = function(source)
+  local stdout_data = {}
+  local stderr_data = {}
+
+  local job = spawn.waitable(source, {
+    on_stdout = function(line)
+      if str.not_empty(line) then
+        table.insert(stdout_data, line)
+      end
+    end,
+    on_stderr = function(line)
+      if str.not_empty(line) then
+        table.insert(stderr_data, line)
+      end
+    end,
+  })
+
+  local completed = spawn.wait(job)
+  return CommandResult:new(stdout_data, stderr_data, completed.exitcode, completed.signal)
+end
+
+-- Get git repository root path.
+--- @return fzfx.CommandResult
+M.run_git_root_sync = function()
+  return M.run_sync({ "git", "rev-parse", "--show-toplevel" })
+end
+
+-- Get git repository branches.
+--- @param remote boolean?
+--- @return fzfx.CommandResult
+M.run_git_branches_sync = function(remote)
+  if remote then
+    return M.run_sync({ "git", "branch", "--remotes" })
+  else
+    return M.run_sync({ "git", "branch" })
+  end
+end
+
+-- Get git repository current branch.
+--- @return fzfx.CommandResult
+M.run_git_current_branch_sync = function()
+  return M.run_sync({ "git", "rev-parse", "--abbrev-ref", "HEAD" })
+end
+
+-- Get git repository remotes.
+--- @return fzfx.CommandResult
+M.run_git_remotes_sync = function()
+  return M.run_sync({ "git", "remote" })
+end
+
+-- Sync Commands {
 
 return M
